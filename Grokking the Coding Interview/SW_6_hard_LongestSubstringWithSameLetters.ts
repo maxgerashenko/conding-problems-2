2a46d481e31608c13cc746eac98ed1dd2489f6e6
maxgerashenko <mgerashchenko@gmail.com>
2022-06-15 11:58:22 -0400

    sw h

--- Grokking the Coding Interview/SW_6_hard_LongestSubstringWithSameLetters.ts
+++ Grokking the Coding Interview/SW_6_hard_LongestSubstringWithSameLetters.ts
@@ -6,17 +6,17 @@
 const length_of_longest_substring = function (str, k) {
     let maxCount = -1;
     let hashMapCount = {};
-    let maxLetterCount = 0;
     let start = 0;
+    let maxLetterCount = 0;
 
     for (let i = 0; i < str.length; i++) {
         let letter = str[i];
         hashMapCount[letter] = hashMapCount[letter] == null ? 1 : hashMapCount[letter] + 1;
         maxLetterCount = Math.max(maxLetterCount, hashMapCount[letter]);
 
-        while (i - start + 1 - maxLetterCount > k) {
+        while(i-start+1-maxLetterCount > k){
             let firstLetter = str[start];
-            hashMapCount[firstLetter] = hashMapCount[firstLetter] - 1;
+            hashMapCount[firstLetter]--;
             start++;
         }
 
@@ -24,7 +24,7 @@ const length_of_longest_substring = function (str, k) {
     }
 
     return maxCount;
-}; // T:O(n) S:O(1) - 26 letters
+}; // T:O(n) S:O(1) K = 26 letters max;
 
 // def length_of_longest_substring(str, k):
 //   # max length
