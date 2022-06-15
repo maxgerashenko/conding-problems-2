2402871bba42d840995f578ce132bb4e56d23d82
maxgerashenko <mgerashchenko@gmail.com>
2022-06-15 00:19:14 -0400

    SW m

--- Grokking the Coding Interview/SW_hard_NoRepeatSubstring.ts
+++ Grokking the Coding Interview/SW_hard_NoRepeatSubstring.ts
@@ -3,33 +3,26 @@
 
 // Given a string, find the length of the longest substring, which has no repeating characters.
 
-const non_repeat_substring = function(str) {
-  // iterate the array
-  // use 2 pointers from 1 side, like a sliding window
-  // use hashMap to remember the last index of the char
-  // if char is in the hashMap,
-  // update start with the current start or char index from the hashMap + 1
-  // update max length as end - start + 1
-  // return the max length
-
-  let max_length = 0;
-
-  let hashMap_index = {};
-
+const non_repeat_substring = function (str) {
+  let max = -1;
+  let hashMapCount = {};
   let start = 0;
-  for (let end = 0; end < str.length; end++) {
-    let char_end = str[end];
 
-    if (hashMap_index[char_end] > start) {
-      start = hashMap_index[char_end] + 1;
+  for (let i = 0; i < str.length; i++) {
+    let letter = str[i];
+    hashMapCount[letter] = hashMapCount[letter] == null ? 1 : hashMapCount[letter] + 1;
+
+    while(hashMapCount[letter] > 1) {
+      let starLetter = str[start];
+      hashMapCount[starLetter] = hashMapCount[starLetter] - 1;
+      start++;
     }
 
-    hashMap_index[char_end] = end;
-    max_length = Math.max(max_length, end - start + 1);
+    max = Math.max(max, +i+1-start);
   }
 
-  return max_length;
-}; // O(N) - O(1) max space in the hashmap is 26 for English letters
+  return max;
+}; // T:O(N+N) S:(1) 26 English letters
 
 // def non_repeat_substring(str):
 //   count = 0
