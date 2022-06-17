91ac7a509aca586b82a1fb2630f36da89bd0c4ce
maxgerashenko <mgerashchenko@gmail.com>
2022-06-17 17:59:16 -0400

    2P m 4

--- Grokking the Coding Interview/2P_3_easy_SquaringSortedArray.ts
+++ Grokking the Coding Interview/2P_3_easy_SquaringSortedArray.ts
@@ -4,17 +4,17 @@
 // Given a sorted array, create a new array containing squares of all the numbers of the input array in the sorted order.
 
 const make_squares = function (arr) {
-  let squares = []
+  let squares = [];
   let start = 0;
   let end = arr.length - 1;
 
   while (start <= end) {
-    if(Math.pow(arr[start], 2) > Math.pow(arr[end], 2)){
-      squares.unshift(Math.pow(arr[start],2));
-      start++
+    if (Math.pow(arr[start], 2) > Math.pow(arr[end], 2)) {
+      squares.unshift(Math.pow(arr[start], 2));
+      start++;
       continue;
     }
-    squares.unshift(Math.pow(arr[end],2));
+    squares.unshift(Math.pow(arr[end], 2));
     end--;
   }
 
