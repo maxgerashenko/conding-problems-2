e73180fdafa4b3827c0cc63a282a7a812265f7dd
maxgerashenko <mgerashchenko@gmail.com>
2022-06-20 18:25:03 -0400

    2P M 9

--- Grokking the Coding Interview/2P_9_medium_QuadrupleSumtoTarget.ts
+++ Grokking the Coding Interview/2P_9_medium_QuadrupleSumtoTarget.ts
@@ -3,45 +3,37 @@
 
 // Given an array of unsorted numbers and a target number, find all unique quadruplets in it, whose sum is equal to the target number.
 
-// Sort array O(N log N)
-// use 1st pointer to iterate use if to filter unique
-// use 2nd pointer to iterate, use while to Filter
-// use 2 pointers to find the taget sum
-
-// def search_quadruplets(arr, target):
-//   arr.sort()
-
-//   quadruples = []
-
-//   for i in range(len(arr)-3):
-//     # filter unique
-//     if arr[i] == arr[i-1]:
-//       i+=1;
-//       continue;
-
-//     for j in range(i+1, len(arr)-2):
-//       # filter unique
-//       while arr[j] == arr[j-1]:
-//         j+=1
-
-//       start = j+1
-//       end = len(arr)-1
-//       while start < end:
-//         sum = arr[i] + arr[j] + arr[start] + arr[end]
-//         if sum == target:
-//           quadruples.append([arr[i], arr[j], arr[start], arr[end]])
-//           start += 1
-//           end -= 1
-//           # filter unique
-//           while start<end and arr[start]==arr[start-1]:
-//             start +=1
-//           # filter unique
-//           while start<end and arr[end] == arr[end+1]:
-//             end -=1
-
-//         if sum > target:
-//           end -= 1
-//         if sum < target:
-//           start += 1
-
-//   return quadruples
+const search_quadruplets = function (arr, target) {
+  arr.sort((x, y) => x - y);
+  const quadruplets = [];
+  for (let i = 0; i < arr.length - 3; i++) {
+    if (arr[i] === arr[i - 1]) continue;
+    for (let j = i + 1; j < arr.length - 2; j++) {
+      if (arr[j] === arr[j - 1]) continue;
+      let sum = arr[i] + arr[j];
+      let start = j + 1;
+      let end = arr.length - 1;
+      while (start < end) {
+        if (arr[start] === arr[start - 1] && start > j + 1) {
+          start++;
+          continue;
+        }
+        if (arr[end] === arr[end + 1]) {
+          end--;
+          continue;
+        }
+        if (arr[start] + arr[end] === target - sum) {
+          quadruplets.push([arr[i], arr[j], arr[start], arr[end]]);
+          start++;
+          end--;
+        }
+        if (arr[start] + arr[end] < target - sum) {
+          start++;
+          continue;
+        }
+        end--;
+      }
+    }
+  }
+  return quadruplets;
+}; // T:O(NlogN + N^3)
