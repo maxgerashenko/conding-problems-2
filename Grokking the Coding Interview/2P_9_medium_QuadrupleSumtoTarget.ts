cbd4ac6f058ea4cf1b5f19a24c9d84e08736f961
maxgerashenko <mgerashchenko@gmail.com>
2022-06-20 19:09:57 -0400

    2P M 9

--- Grokking the Coding Interview/2P_9_medium_QuadrupleSumtoTarget.ts
+++ Grokking the Coding Interview/2P_9_medium_QuadrupleSumtoTarget.ts
@@ -3,16 +3,20 @@
 
 // Given an array of unsorted numbers and a target number, find all unique quadruplets in it, whose sum is equal to the target number.
 
+// 2 for
+// 1 while
+// 4 check of d
+
 const search_quadruplets = function (arr, target) {
   arr.sort((x, y) => x - y);
   const quadruplets = [];
   for (let i = 0; i < arr.length - 3; i++) {
     if (arr[i] === arr[i - 1]) continue;
     for (let j = i + 1; j < arr.length - 2; j++) {
-      if (arr[j] === arr[j - 1]) continue;
-      let sum = arr[i] + arr[j];
+      if (arr[j] === arr[j - 1] && j > i + 1) continue;
       let start = j + 1;
       let end = arr.length - 1;
+      let sum = arr[i] + arr[j];
       while (start < end) {
         if (arr[start] === arr[start - 1] && start > j + 1) {
           start++;
@@ -22,12 +26,14 @@ const search_quadruplets = function (arr, target) {
           end--;
           continue;
         }
-        if (arr[start] + arr[end] === target - sum) {
+        let current = sum + arr[start] + arr[end];
+        if (current === target) {
           quadruplets.push([arr[i], arr[j], arr[start], arr[end]]);
           start++;
           end--;
+          continue;
         }
-        if (arr[start] + arr[end] < target - sum) {
+        if (current < target) {
           start++;
           continue;
         }
@@ -36,4 +42,4 @@ const search_quadruplets = function (arr, target) {
     }
   }
   return quadruplets;
-}; // T:O(NlogN + N^3)
+}; // T(NlogN + N*N*N) S:O(N)
