22c1f499c54e427cb9b2ae04085a0dad6b566791
maxgerashenko <mgerashchenko@gmail.com>
2022-06-23 00:02:24 -0400

    FS M 3

--- Grokking the Coding Interview/FS_2_medium_StartLinkedListCycle.ts
+++ Grokking the Coding Interview/FS_2_medium_StartLinkedListCycle.ts
@@ -3,16 +3,15 @@
 
 // Given the head of a Singly LinkedList that contains a cycle, write a function to find the starting node of the cycle.
 
-const find_cycle_start = function (head) {
-  // use Floyds algorithm to find the start of the cycle
-  // use slow and fast pointer to find a meeting point
-  // the distance between the meeting point and cycle start
-  // and start of the list and cycle start is equal
-  // - use 2 pointers fast and slow to find a meeting point
-  // - set start to the list start
-  // - iterate start and end from meeting point
-  // - when start will equal meeting point
-  // - then end will equal cycle start
+// use Floyds algorithm to find the start of the cycle
+// use slow and fast pointer to find a meeting point
+// the distance between the meeting point and cycle start
+// and start of the list and cycle start is equal
+// - use 2 pointers fast and slow to find a meeting point
+// - set start to the list start
+// - iterate start and end from meeting point
+// - when start will equal meeting point
+// - then end will equal cycle start
 
 const find_cycle_start = function (head) {
   let slow = head && head.next;
--- Grokking the Coding Interview/FS_3_medium_HappyNumber.ts
+++ Grokking the Coding Interview/FS_3_medium_HappyNumber.ts
@@ -3,40 +3,25 @@
 
 // Any number will be called a happy number if, after repeatedly replacing it with a number equal to the sum of the square of all of its digits, leads us to number ‘1’. All other (not-happy) numbers will never reach ‘1’. Instead, they will be stuck in a cycle of numbers which does not include ‘1’.
 
-const find_happy_number = function(num) {
   // take number
   // split to digits
   // get squared
   // create new node point to the new node
   // use 2 pointers from 1 side
   // fast and slow
+const find_happy_number = function (num) {
   // if fast is 1 it is happy number
   // if fast === slow it is cycle
 
-  let fast = num;
+ const next = number => String(number).split('').map(num => Math.pow(+num, 2)).reduce((res, num) => res + num, 0);
+const find_happy_number = function (num) {
   let slow = num;
-
-  let step = num => {
-    let sum = 0;
-    while (num !== 0) {
-      let digit = num % 10;
-      sum += Math.pow(digit, 2);
-      num = Math.floor(num / 10);
-    }
-
-    return sum;
-  };
-
-  // iterate fast and slow pointers
-  while (true) {
-    slow = step(slow);
-    fast = step(fast);
-    fast = step(fast);
-
-    // stop if pointers are equal
-    if (slow === fast) break;
+  let fast = num;
+  while (slow !== 1) {
+    slow = next(slow);
+    fast = next(next(fast));
+    if (slow !== 1 && slow == fast) return false;
   }
 
-  // check if pointers equals 1, then it is a happy number
-  return fast === 1;
-}; // O(logN)
+  return true;
+}; // T:O(LogN) S:O(1)
