87ab046fd8291d9eabd54d3a181e89063c2af07b
maxgerashenko <mgerashchenko@gmail.com>
2022-06-23 23:57:55 -0400

    FS M 5

--- Grokking the Coding Interview/FS_5_medium_PalindromeLinkedList.ts
+++ Grokking the Coding Interview/FS_5_medium_PalindromeLinkedList.ts
@@ -4,31 +4,32 @@
 // Given the head of a Singly LinkedList, write a method to check if the LinkedList is a palindrome or not.
 // Your algorithm should use constant space and the input LinkedList should be in the original form once the algorithm is finished. The algorithm should have O(N)O(N) time complexity where ‘N’ is the number of nodes in the LinkedList.
 
-const is_palindromic_linked_list = function (head) {
-  let slow = head;
-  let fast = head;
+// while fast.next && fast.next.next != 
 
-  while (fast != null && fast.next != null) {
+const is_palindromic_linked_list = function (head) {
+  let fast = head;
+  let slow = head;
+  while (fast.next !== null && fast.next.next !== null) {
     slow = slow.next;
     fast = fast.next.next;
   }
 
   let pre = slow;
-  let fixed = slow.next;
-  while (fixed.next != null) {
-    let tmp = fixed.next;
-    fixed.next = tmp.next;
+  let start = slow.next;
+  while (start.next != null) {
+    let tmp = start.next;
+    start.next = tmp.next;
     tmp.next = pre.next;
     pre.next = tmp;
   }
 
-  let start = head;
-  let middle = fast == null ? pre : pre.next;
-  while (start.value === middle.value) {
-    start = start.next;
-    middle = middle.next;
-    if (middle.next == null) return true;
+  let first = head;
+  let second = pre.next;
+  while (second != null) {
+    if (first.value !== second.value) return false;
+    first = first.next;
+    second = second.next;
   }
 
-  return false;
-}; // T:O(N)  S:O(1)
+  return true;
+}; // T:O(N) S:O(1)
