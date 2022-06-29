eb24e93b42277ffaae1f8ffcedd106004fef9bec
maxgerashenko <mgerashchenko@gmail.com>
2022-06-29 11:56:37 -0400

    MI 2

--- Grokking the Coding Interview/MI_2_medium_InsertIntervals.ts
+++ Grokking the Coding Interview/MI_2_medium_InsertIntervals.ts
@@ -1,28 +1,15 @@
 // https://www.educative.io/courses/grokking-the-coding-interview/3jKlyNMJPEM
 // Insert intervals
 
-const insert = function (intervals, new_interval) {
-  let { start: min, end: max } = new_interval;
-  let merged = [];
-
-  for (let interval of intervals) {
-    let { start, end } = interval;
-    if (end < min) {
-      merged.push(interval);
-      continue;
+const merge = function(intervals_a, intervals_b) {
+  let result = [];
+  let b = intervals_b.shift();
+  for(let a of intervals_a){
+    if(a.end < b.start) continue;
+    while(b.end < a.start) {
+      b = intervals_b.shift();
     }
-    if (start > max) {
-      merged.push(new Interval(min, max));
-      min = null;
-      max = null;
-      merged.push(interval);
-      continue;
-    }
-    min = Math.min(min, start);
-    max = Math.max(max, end);
+    result.push(new Interval(Math.max(a.start, b.start), Math.min(a.end, b.end)));
   }
-
-  if (min && max) merged.push(new Interval(min, max));
-
-  return merged;
-}; // T:O(N) S:O(N)
+  return result;
+}; // T:O(N+M) S:O(N|M)
