fac7b290bd2c0ab8a78cf82dee0f4e1302f651f1
maxgerashenko <mgerashchenko@gmail.com>
2022-06-29 22:40:49 -0400

    MI 5

--- Grokking the Coding Interview/MI_4_hard_MinimumMeetingRooms.ts
+++ Grokking the Coding Interview/MI_4_hard_MinimumMeetingRooms.ts
@@ -5,32 +5,32 @@
 // T:O(NlogN + N S:O(N)
 
 class Heap {
-  constructor(sort){
+  constructor(sort) {
     this.sort = sort;
-    this.arr= [];
+    this.arr = [];
   }
-  add(val){
+  add(val) {
     this.arr.push(val);
     this.arr.sort(this.sort);
   }
-  pop(){
+  pop() {
     this.arr.shift();
   }
-  getEl(){
+  getEl() {
     return this.arr[0];
   }
-  getLen(){
+  getLen() {
     return this.arr.length;
   }
 }
 
-const min_meeting_rooms = function(meetings) {
+const min_meeting_rooms = function (meetings) {
   let maxCount = 0;
-  meetings.sort((x,y)=> x.start - y.start);
-  let minHeap = new Heap((x,y)=>x.end-y.end);
+  meetings.sort((x, y) => x.start - y.start);
+  let minHeap = new Heap((x, y) => x.end - y.end);
   minHeap.add(meetings.shift());
-  for(meeting of meetings){
-    while(minHeap.getEl() && minHeap.getEl().end <= meeting.start){
+  for (meeting of meetings) {
+    while (minHeap.getEl() && minHeap.getEl().end <= meeting.start) {
       minHeap.pop();
     }
     minHeap.add(meeting);
---
+++ Grokking the Coding Interview/MI_5_hard_MaximumCPULoad.ts
@@ -0,0 +1,42 @@
+// https://www.educative.io/courses/grokking-the-coding-interview/xVlyyv3rR93
+// Maximum CPU Load
+
+// We are given a list of Jobs. Each job has a Start time, an End time, and a CPU load when it is running. Our goal is to find the maximum CPU load at any time if all the jobs are running on the same machine.
+
+
+class Heap {
+  constructor(sort) {
+    this.sort = sort;
+    this.arr = [];
+  }
+  add(el) {
+    this.arr.push(el);
+    this.arr.sort((x, y) => this.sort);
+  }
+  remove() {
+    return this.arr.pop();
+  }
+  getEl() {
+    return this.arr[0];
+  }
+  getLength() {
+    return this.arr.length();
+  }
+  getTotalLoad() {
+    return this.arr.reduce((res, cur) => res + cur.cpu_load, 0);
+  }
+}
+
+const find_max_cpu_load = function (jobs) {
+  jobs.sort((x, y) => x.start - y.start);
+  let maxLoad = 0;
+  let minHeap = new Heap((x, y) => x.end - y.end);
+  for (let job of jobs) {
+    while (minHeap.getEl() && minHeap.getEl().end <= job.start) {
+      minHeap.remove();
+    }
+    minHeap.add(job);
+    maxLoad = Math.max(maxLoad, minHeap.getTotalLoad());
+  }
+  return maxLoad;
+}; // T:O(NlonN) S:O(1)
