a3a6aeac28883f3ccfe2cc25ed62e0ba3b453dd2
maxgerashenko <mgerashchenko@gmail.com>
2022-07-09 00:10:02 -0400

    2H up

--- Grokking the Coding Interview/2H_4_?_hard_NextInterval.ts
+++ Grokking the Coding Interview/2H_4_?_hard_NextInterval.ts
@@ -2,25 +2,25 @@
 //
 // Next Interval
 
-const find_next_interval = function(intervals) {
+const find_next_interval = function (intervals) {
   let result = [];
-  let minStarts = [...intervals.sort((x,y)=>x.start-y.start)];
-  let minEnds = [...intervals.sort((x,y)=>x.end-y.end)];
+  let minStarts = [...intervals.sort((x, y) => x.start - y.start)];
+  let minEnds = [...intervals.sort((x, y) => x.end - y.end)];
   let mapIndex = new Map();
-  for(let i in intervals){
+  for (let i in intervals) {
     mapIndex.set(intervals[i], i);
   }
-  for(let minEnd of minEnds){
+  for (let minEnd of minEnds) {
     let index = mapIndex.get(minEnd);
-    for(let i in minStarts){
+    for (let i in minStarts) {
       let minStart = minStarts[i];
-      if(minEnd.end > minStart.start) continue;
+      if (minEnd.end > minStart.start) continue;
       result[index] = mapIndex.get(minStart);
       break;
     }
-    if(result[index] == null) {
+    if (result[index] == null) {
       result[index] = -1;
     }
   }
-  return result
+  return result;
 }; // T:O(NlogN) S:O(N)
