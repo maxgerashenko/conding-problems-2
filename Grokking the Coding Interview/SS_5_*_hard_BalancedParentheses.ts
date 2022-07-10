1127c8baf0d23b2ea78c48218deeba7ec6c87f4e
maxgerashenko <mgerashchenko@gmail.com>
2022-07-10 00:06:33 -0400

    SS 5

--- Grokking the Coding Interview/SS_5_*_hard_BalancedParentheses.ts
+++ Grokking the Coding Interview/SS_5_*_hard_BalancedParentheses.ts
@@ -3,22 +3,15 @@
 // Balanced Parentheses
 
 const generate_valid_parentheses = function (num) {
-  let combs = [{ comb: ['('], openCount: num - 1, balance: 1 }];
-  for (let n = 0; n < num * 2 - 1; n++) {
+  let combs = [{ comb: '(', open: num - 1, bal: 1 }];
+  for (let n = 1; n < num * 2; n++) {
     let copy = [];
-    for (let { comb, openCount, balance } of combs) {
-      if (openCount > 0) {
-        copy.push({
-          comb: [...comb, '('],
-          openCount: openCount - 1,
-          balance: balance + 1,
-        });
-      }
-      if (balance > 0) {
-        copy.push({ comb: [...comb, ')'], openCount, balance: balance - 1 });
-      }
+    for (let { comb, open, bal } of combs) {
+      if (open > 0)
+        copy.push({ comb: comb + '(', open: open - 1, bal: bal + 1 });
+      if (bal > 0) copy.push({ comb: comb + ')', open, bal: bal - 1 });
     }
-    combs = [...copy];
+    combs = copy;
   }
-  return combs.map((el) => el.comb.join(''));
-}; // T:O(N*2^N) S:O(N*2^N)
+  return combs.map((el) => el.comb);
+}; //T:O(N*2^N) S:O(N*2^N)
