08035ad07e1ac185fae51a83228f4e32a3477450
maxgerashenko <mgerashchenko@gmail.com>
2022-07-19 01:04:41 -0400

    SS 8

--- Grokking the Coding Interview/SS_8_***_hard_Unique_Binary_Search_Trees.ts
+++ Grokking the Coding Interview/SS_8_***_hard_Unique_Binary_Search_Trees.ts
@@ -2,16 +2,17 @@
 //
 // Structurally Unique Binary Search Trees
 
-const find_unique_trees = function (end, start = 1, result = []) {
-  if (start > end) return [null];
+const find_unique_trees = function (end, start = 1, results = []) {
+  if (start > end) return [null]; // base case
   for (let i = start; i <= end; i++) {
     let left = find_unique_trees(i - 1, start);
     let right = find_unique_trees(end, i + 1);
     for (let l of left) {
       for (let r of right) {
-        result.push(new TreeNode(i, l, r));
+        let node = new TreeNode(i, l, r);
+        results.push(node);
       }
     }
   }
-  return result;
-}; // T:O(N2^N) S:O(2^N) No Memoization possible
+  return results;
+}; // T:O(N2^N) S:O(2^N)  No Memoization possible
