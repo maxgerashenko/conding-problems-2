ef263bbb8ee41f8e73fa755b0b52d57eac88aaec
maxgerashenko <mgerashchenko@gmail.com>
2022-07-19 01:24:01 -0400

    ss 9

--- Grokking the Coding Interview/SS_9_**_hard_CountOfUniqueBST.ts
+++ Grokking the Coding Interview/SS_9_**_hard_CountOfUniqueBST.ts
@@ -2,14 +2,14 @@
 //
 // Count of Structurally Unique Binary Search Trees
 
-function count_trees(n, hashMap = {}, count = 0) {
-  if (hashMap[n]) return hashMap[n];
-  if (n <= 1) return 1; // base case;
-  for (let i = 1; i <= n; i++) {
-    let left = count_trees(i - 1, hashMap);
-    let right = count_trees(n - i, hashMap);
+const count_trees = function (end, start = 1, map = {}, count = 0) {
+  if (map[end - start] != null) return map[end - start]; // memoizetion
+  if (start > end) return 1; // base case
+  for (let i = start; i <= end; i++) {
+    let left = count_trees(i - 1, start, map);
+    let right = count_trees(end, i + 1, map);
     count += left * right;
   }
-  hashMap[n] = count;
+  map[end - start] = count;
   return count;
-} // T:O(2^N) S:O(2^N) ; memp T:O(2^N) S:O(N)
+}; // T:O(N2^N) S:O(2^N) memo T:O(2^N) S:O(N)
