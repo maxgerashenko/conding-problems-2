74b6a587ba4831a74e01934a51c03559acf571cc
maxgerashenko <mgerashchenko@gmail.com>
2022-07-26 22:32:14 -0400

    TOPK 7

--- Grokking the Coding Interview/TOPK_7_medium_LargesInAStream.ts
+++ Grokking the Coding Interview/TOPK_7_medium_LargesInAStream.ts
@@ -2,40 +2,31 @@
 //
 // Largest Number in a Stream
 
+class Heap {
+  constructor(sort) {
+    this.sort = sort;
+    this.arr = [];
+    this.push = (el) =>
+      (this.arr = [...this.arr, el].sort((a, b) => this.sort(a, b)));
+    this.pop = () => this.arr.shift();
+  }
+}
 class KthLargestNumberInStream {
   constructor(nums, k) {
-    this.min = new Heap();
-    for (let i = 0; i < k; i++) {
-      this.min.push(nums.shift());
-    }
-
-    for (let num of nums) {
-      if (num <= this.min.arr[0]) continue;
-      this.min.pop();
-      this.min.push(num);
-    }
+    this.k = k;
+    this.minHeap = new Heap((x, y) => x - y);
+    for (let num of nums) this.add(num);
   }
 
   add(num) {
-    if (num <= this.min.arr[0]) return this.min.arr[0];
-    this.min.pop();
-    this.min.push(num);
-    return this.min.arr[0];
+    if (this.minHeap.arr.length < this.k) {
+      this.minHeap.push(num);
+      return this.minHeap.arr[0];
+    }
+    if (num > this.minHeap.arr[0]) {
+      this.minHeap.push(num);
+      this.minHeap.pop(num);
+    }
+    return this.minHeap.arr[0];
   }
-} // T:O(KlogK + (N-K)logK) S:O(K)
-
-class Heap {
-  constructor(isMax = false) {
-    this.arr = [];
-    this.sort = isMax ? (a, b) => b - a : (a, b) => a - b;
-  }
-  push(el) {
-    this.arr.push(el);
-    this.arr.sort(this.sort);
-  }
-  pop() {
-    let tmp = this.arr.shift();
-    this.arr.sort(this.sort);
-    return tmp;
-  }
-}
+} // T:O(logK) S:O(K)
