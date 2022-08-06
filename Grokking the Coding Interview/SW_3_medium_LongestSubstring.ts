035d969e74aa99d6a6a7fea93396aff33ccb7dc4
maxgerashenko <mgerashchenko@gmail.com>
2022-08-05 23:20:45 -0400

    SW 4

--- Grokking the Coding Interview/SW_3_medium_LongestSubstring.ts
+++ Grokking the Coding Interview/SW_3_medium_LongestSubstring.ts
@@ -4,20 +4,25 @@
 // Given a string, find the length of the longest substring in it with no more than K distinct characters.
 // You can assume that K is less than or equal to the length of the given string.
 
-const longest_substring_with_k_distinct = function (str, k) {
-  let start = 0;
-  let end = 0;
-  let hashMapCount = {};
-  let max = 0;
-  for (end = 0; end < str.length; end++) {
+const longest_substring_with_k_distinct = function (
+  str,
+  k,
+  max = 0,
+  start = 0,
+  end = 0,
+  hashMapCount = {}
+) {
+  hashMapCount[str[start]] = 1;
+  while (start <= end && end < str.length) {
+    max = Math.max(max, end - start + 1);
+    end++;
     hashMapCount[str[end]] =
-      hashMapCount[str[end]] == null ? 1 : hashMapCount[str[end]] + 1;
-    while (Object.keys(hashMapCount).length > k) {
+      hashMapCount[str[end]] == null ? 1 : hashMapCount[str[end]]++;
+    while (start <= end && Object.keys(hashMapCount).length > k) {
       hashMapCount[str[start]]--;
       if (hashMapCount[str[start]] === 0) delete hashMapCount[str[start]];
       start++;
     }
-    max = Math.max(max, end - start + 1);
   }
   return max;
-}; // T:O(N+K) S:O(K+1)
+}; // T:O(N) S:O(K)
--- Grokking the Coding Interview/SW_4_*_medium_FruitsIntoBaskets.ts
+++ Grokking the Coding Interview/SW_4_*_medium_FruitsIntoBaskets.ts
@@ -5,22 +5,28 @@
 // You can start with any tree, but you can’t skip a tree once you have started. You will pick one fruit from each tree until you cannot, i.e., you will stop when you have to pick from a third fruit type.
 // Write a function to return the maximum number of fruits in both baskets.
 
-const fruits_into_baskets = function (fruits) {
-  let max = 0;
-  let hashMapCount = {};
-  let start = 0;
-  for (let el of fruits) {
-    hashMapCount[el] = hashMapCount[el] == null ? 1 : hashMapCount[el] + 1;
-
-    while (Object.keys(hashMapCount).length > 2) {
+const fruits_into_baskets = function (
+  fruits,
+  start = 0,
+  end = 0,
+  count = 1,
+  max = 1,
+  k = 2,
+  hashMapCount = {}
+) {
+  hashMapCount[fruits[start]] = 1;
+  while (end < fruits.length) {
+    end++;
+    count++;
+    hashMapCount[fruits[end]] =
+      hashMapCount[fruits[end]] == null ? 1 : hashMapCount[fruits[end]] + 1;
+    while (start <= end && Object.keys(hashMapCount).length > k) {
       hashMapCount[fruits[start]]--;
       if (hashMapCount[fruits[start]] === 0) delete hashMapCount[fruits[start]];
       start++;
+      count--;
     }
-    max = Math.max(
-      max,
-      Object.values(hashMapCount).reduce((pre, cur) => pre + cur, 0)
-    );
+    max = Math.max(max, count);
   }
   return max;
-}; // T:O(N+K) S:O(K)
+}; // T:O(N) S:O(K)
