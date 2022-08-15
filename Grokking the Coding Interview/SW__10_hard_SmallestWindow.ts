1d77dae35d4439a685026497565ea98192962315
maxgerashenko <mgerashchenko@gmail.com>
2022-08-14 22:23:29 -0400

    Names update

--- Grokking the Coding Interview/SW__10_hard_SmallestWindow.ts
+++ Grokking the Coding Interview/SW__10_hard_SmallestWindow.ts
@@ -1,6 +1,6 @@
 // https://www.educative.io/courses/grokking-the-coding-interview/Y5YDWzqPn7O
 //
-// Words Concatenation
+// Smallest Window containing Substring
 
 // Given a string and a list of words, find all the starting indices of substrings in the given string that are a concatenation of all the given words exactly once without any overlapping of words. It is given that all words are of the same length.
 
