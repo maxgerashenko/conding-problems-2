cb31141b851e3e6f2f12038e3e8d855bac1a8458
maxgerashenko <mgerashchenko@gmail.com>
2022-08-24 00:53:54 -0400

    CS 2

--- Grokking the Coding Interview/CS_7_*_medium_MissingPositiveNumber.ts
+++ Grokking the Coding Interview/CS_7_*_medium_MissingPositiveNumber.ts
@@ -10,16 +10,11 @@
 
 // conner case
 const find_first_smallest_missing_positive = function (nums) {
-  for (let i in nums) {
-    while (nums[i] !== +i && nums[i] >= 0) {
+  for (let i = 0; i < nums.length; i++)
+    while (nums[i] != null && nums[i] >= 0 && nums[i] != i) {
       let val = nums[i];
       [nums[i], nums[val]] = [nums[val], nums[i]];
     }
-  }
-
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] !== +i || nums[i] == null) return +i;
-  }
-
+  for (let i = 0; i < nums.length; i++) if (i > 0 && i != nums[i]) return i;
   return -1;
-};
+}; // T:O(N) S:O(1)
