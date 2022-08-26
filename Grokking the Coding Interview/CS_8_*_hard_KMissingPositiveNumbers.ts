cdc894d863c90f5a9a3b6a0afee558e0a0992a92
maxgerashenko <mgerashchenko@gmail.com>
2022-08-26 10:58:04 -0400

    CS 8

--- Grokking the Coding Interview/CS_8_*_hard_KMissingPositiveNumbers.ts
+++ Grokking the Coding Interview/CS_8_*_hard_KMissingPositiveNumbers.ts
@@ -14,12 +14,13 @@
 // find [i] !== i+1 and >0 and not it ther set
 
 const find_first_k_missing_positive = function (nums, k, results = []) {
-  for (let i = 0; i < nums.length; i++)
-    while (i != nums[i]) {
+  for (let i = 0; i < nums.length; i++) {
+    while (nums[i] != i) {
       let val = nums[i];
       if (nums[i] == null || nums[i] < 0 || nums[i] == nums[val]) break;
       [nums[i], nums[val]] = [nums[val], nums[i]];
     }
+  }
   let i = 1;
   while (k > 0) {
     if (nums[i] != i) {
