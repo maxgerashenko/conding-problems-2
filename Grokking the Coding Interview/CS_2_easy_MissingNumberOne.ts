a147b4d75a59f7f4bd872feb7d094897fafb9d87
maxgerashenko <mgerashchenko@gmail.com>
2022-08-26 09:52:08 -0400

    CS 2

--- Grokking the Coding Interview/CS_2_easy_MissingNumberOne.ts
+++ Grokking the Coding Interview/CS_2_easy_MissingNumberOne.ts
@@ -3,11 +3,12 @@
 
 const find_missing_number = function (nums) {
   for (let i = 0; i < nums.length; i++) {
-    while (nums[i] != i && nums[i] != null) {
+    while (nums[i] != i) {
       let val = nums[i];
+      if (nums[i] == null) break;
       [nums[i], nums[val]] = [nums[val], nums[i]];
     }
   }
   for (let i = 0; i < nums.length; i++) if (nums[i] == null) return i;
   return -1;
-}; // T:O(N) S:O(1)
+}; // T:O(N S:O(1)
