f10045c5b3512919798e714437e0a6dd32a1a2fb
maxgerashenko <mgerashchenko@gmail.com>
2022-08-26 10:28:22 -0400

    CS 6

--- Grokking the Coding Interview/CS_6_**_easy_FindTheCorruptPair.ts
+++ Grokking the Coding Interview/CS_6_**_easy_FindTheCorruptPair.ts
@@ -1,14 +1,14 @@
 // https://www.educative.io/courses/grokking-the-coding-interview/mE2LVDE3wp0
 // Find the Corrupt Pair
 
-const find_corrupt_numbers = function (nums, results = []) {
+const find_corrupt_numbers = function (nums) {
   for (let i = 0; i < nums.length; i++)
-    while (i + 1 != nums[i]) {
+    while (nums[i] != i + 1) {
       let val = nums[i];
-      if (nums[i] === nums[val - 1]) break;
+      if (nums[i] == nums[val - 1]) break;
       [nums[i], nums[val - 1]] = [nums[val - 1], nums[i]];
     }
   for (let i = 0; i < nums.length; i++)
-    if (nums[i] != i + 1) results.push(nums[i], i + 1);
-  return results;
-}; // T:O(N) S:O(1)
+    if (nums[i] != i + 1) return [nums[i], i + 1];
+  return [-1, -1];
+}; // T:O(N) S:O(1);
