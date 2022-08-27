bf6cfc1e9b01c4ff081d290da0b781d94b3c8d69
maxgerashenko <mgerashchenko@gmail.com>
2022-08-27 09:40:42 -0400

    CS 3

--- Grokking the Coding Interview/CS_3_**_easy_MissingNumbersAll.ts
+++ Grokking the Coding Interview/CS_3_**_easy_MissingNumbersAll.ts
@@ -11,13 +11,12 @@
 
 // conners case
 const find_missing_numbers = function (nums, results = []) {
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length; i++)
     while (nums[i] != i + 1) {
       let val = nums[i];
-      if (nums[i] == nums[val - 1]) break;
+      if (nums[i] == null || nums[i] == nums[val - 1]) break;
       [nums[i], nums[val - 1]] = [nums[val - 1], nums[i]];
     }
-  }
   for (let i = 0; i < nums.length; i++)
     if (nums[i] != i + 1) results.push(i + 1);
   return results;
