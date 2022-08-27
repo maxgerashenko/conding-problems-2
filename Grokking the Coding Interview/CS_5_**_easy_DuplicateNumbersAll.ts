43aa004870004e8bb733e89fcf1dec0b8f0f90c9
maxgerashenko <mgerashchenko@gmail.com>
2022-08-27 09:46:56 -0400

    CS 5

--- Grokking the Coding Interview/CS_5_**_easy_DuplicateNumbersAll.ts
+++ Grokking the Coding Interview/CS_5_**_easy_DuplicateNumbersAll.ts
@@ -2,15 +2,14 @@
 // Find all Duplicate Numbers
 
 const find_all_duplicates = function (nums, results = []) {
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length; i++)
     while (nums[i] != i + 1) {
       let val = nums[i];
-      if (nums[i] == nums[val - 1]) {
-        results.push(nums[i]);
-        break;
-      }
+      if (nums[i] == null || nums[i] == nums[val - 1]) break;
       [nums[i], nums[val - 1]] = [nums[val - 1], nums[i]];
     }
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != i + 1 && nums[i] != null) results.push(nums[i]);
   }
   return results;
 }; // T:O(N) S:O(1)
