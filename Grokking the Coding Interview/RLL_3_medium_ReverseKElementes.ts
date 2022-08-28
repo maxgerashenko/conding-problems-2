98e4d28d713b754a8890997fa98ccf04065d6408
maxgerashenko <mgerashchenko@gmail.com>
2022-08-28 09:53:04 -0400

    RLL 3

--- Grokking the Coding Interview/RLL_3_medium_ReverseKElementes.ts
+++ Grokking the Coding Interview/RLL_3_medium_ReverseKElementes.ts
@@ -4,13 +4,15 @@
 // Given the head of a LinkedList and a number ‘k’, reverse every ‘k’ sized sub-list starting from the head.
 // If, in the end, you are left with a sub-list with less than ‘k’ elements, reverse it too.
 
-const reverse_every_k_elements = function (head, k) {
-  let root = {};
-  root.next = head;
-  let pre = root;
-  while (pre.next) {
-    let count = k - 1;
+const reverse_every_k_elements = function (
+  head,
+  k,
+  pre = { next: head },
+  root = pre
+) {
+  while (pre && pre.next) {
     let start = pre.next;
+    let count = k - 1;
     while (start.next && count > 0) {
       let tmp = start.next;
       start.next = tmp.next;
@@ -18,7 +20,7 @@ const reverse_every_k_elements = function (head, k) {
       pre.next = tmp;
       count--;
     }
-    pre = start;
+    pre = start.next;
   }
   return root.next;
 }; // T:O(N) S:O(1)
