02bc96ea178d5b1d5d84cf6085d39a50ca0dfd2f
maxgerashenko <mgerashchenko@gmail.com>
2022-08-28 20:03:18 -0400

    MI 2

--- Grokking the Coding Interview/MI_2_*_medium_InsertIntervals.ts
+++ Grokking the Coding Interview/MI_2_*_medium_InsertIntervals.ts
@@ -1,25 +1,21 @@
 // https://www.educative.io/courses/grokking-the-coding-interview/3jKlyNMJPEM
 // Insert intervals
 
-const insert = function (intervals, new_interval) {
-  let merged = [];
-  let { start: newStart, end: newEnd } = new_interval;
-  let i = 0;
-  while (i < intervals.length && intervals[i].end < newStart) {
-    merged.push(intervals[i]);
-    i++;
+const insert = function (intervals, { start, end }, results = [], i) {
+  for (i = 0; i < intervals.length; i++) {
+    let el = intervals[i];
+    if (el.end < start) {
+      results.push(el);
+      continue;
+    } // before
+    if (el.start <= end) {
+      start = Math.min(start, el.start);
+      end = Math.max(end, el.end);
+      continue;
+    } // merge
+    if (end < el.start) break;
   }
-  while (i < intervals.length && intervals[i].start < newEnd) {
-    newStart = Math.min(newStart, intervals[i].start);
-    newEnd = Math.max(newEnd, intervals[i].end);
-    i++;
-  }
-  merged.push(new Interval(newStart, newEnd));
-
-  while (i < intervals.length) {
-    merged.push(intervals[i]);
-    i++;
-  }
-
-  return merged;
-}; // T:O(N) S:(N + K)
+  results.push(new Interval(start, end)); // add
+  results.push(...intervals.slice(i)); // after
+  return results;
+}; // T:O(N) S:O(N)
