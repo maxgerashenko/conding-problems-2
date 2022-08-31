ed4d483e13752a76e035aecc8d60d7edc43ca67c
maxgerashenko <mgerashchenko@gmail.com>
2022-08-30 23:44:20 -0400

    MI 2

--- Grokking the Coding Interview/MI_2_*_medium_InsertIntervals.ts
+++ Grokking the Coding Interview/MI_2_*_medium_InsertIntervals.ts
@@ -1,21 +1,22 @@
 // https://www.educative.io/courses/grokking-the-coding-interview/3jKlyNMJPEM
 // Insert intervals
 
-const insert = function (intervals, { start, end }, results = [], i) {
-  for (i = 0; i < intervals.length; i++) {
-    let el = intervals[i];
-    if (el.end < start) {
-      results.push(el);
-      continue;
-    } // before
-    if (el.start <= end) {
-      start = Math.min(start, el.start);
-      end = Math.max(end, el.end);
-      continue;
-    } // merge
-    if (end < el.start) break;
+const insert = function (
+  intervals,
+  { start: iStart, end: iEnd },
+  i = 0,
+  results = []
+) {
+  for (i; i < intervals.length; i++) {
+    let { start, end } = intervals[i];
+    if (end >= iStart) break;
+    results.push(new Interval(start, end)); // before
   }
-  results.push(new Interval(start, end)); // add
-  results.push(...intervals.slice(i)); // after
-  return results;
+  for (i; i < intervals.length; i++) {
+    let { start, end } = intervals[i];
+    if (iEnd < start) break;
+    iEnd = Math.max(iEnd, end);
+  } // merged
+  results.push(new Interval(iStart, iEnd)); // insert merged
+  return [...results, ...intervals.slice(i)]; // rest
 }; // T:O(N) S:O(N)
