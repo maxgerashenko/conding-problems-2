49365b7fc16a4e732bc1ab6521916bb936aa4849
maxgerashenko <mgerashchenko@gmail.com>
2022-08-30 23:59:27 -0400

    MI 3

--- Grokking the Coding Interview/MI_3_**_medium_IntervalsIntersection.ts
+++ Grokking the Coding Interview/MI_3_**_medium_IntervalsIntersection.ts
@@ -3,25 +3,22 @@
 
 // Given two lists of intervals, find the intersection of these two lists. Each list consists of disjoint intervals sorted on their start time.
 
-const merge = function (intervalsA, intervalsB, results = [], i = 0, j = 0) {
-  while (i < intervalsA.length && j < intervalsB.length) {
-    let isAinB =
-      intervalsA[i].start <= intervalsB[j].start &&
-      intervalsA[i].end >= intervalsB[j].start;
-    let isBinA =
-      intervalsB[j].start <= intervalsA[i].start &&
-      intervalsB[j].end >= intervalsA[i].start;
-
+const merge = function (intA, intB, results = [], i = (j = 0)) {
+  while (i < intA.length && j < intB.length) {
+    let a = intA[i];
+    let b = intB[j];
+    let isAinB = a.start <= b.start && a.end >= b.start;
+    let isBinA = b.start <= a.start && b.end >= a.start;
     if (isAinB || isBinA) {
-      let start = Math.max(intervalsA[i].start, intervalsB[j].start);
-      let end = Math.min(intervalsA[i].end, intervalsB[j].end);
+      let start = Math.max(a.start, b.start);
+      let end = Math.min(a.end, b.end);
       results.push(new Interval(start, end));
     }
-    if (intervalsA[i].end < intervalsB[j].end) {
+    if (a.end < b.end) {
       i++;
       continue;
     }
     j++;
   }
   return results;
-}; // T:O(N) S:O(N)
+}; // T:O(N) S:(N)
