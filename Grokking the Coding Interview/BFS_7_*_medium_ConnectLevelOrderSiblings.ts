554dcf2650ab6c5cfac5e3d22eb138150bda73cf
maxgerashenko <mgerashchenko@gmail.com>
2022-09-02 01:15:47 -0400

    BFS 7

--- Grokking the Coding Interview/BFS_7_*_medium_ConnectLevelOrderSiblings.ts
+++ Grokking the Coding Interview/BFS_7_*_medium_ConnectLevelOrderSiblings.ts
@@ -14,25 +14,19 @@
 // set null at the end of the level
 // return root
 
-const connect_level_order_siblings = function (root) {
-  let nodes = [root];
-  while (nodes.length) {
-    let level = [];
+const connect_level_order_siblings = function (root, level = [root]) {
+  while (level.length) {
+    let tmp = [];
     let pre = null;
-    for (let { left, right } of nodes) {
-      if (pre && left) pre.next = left;
-      if (left) {
-        level.push(left);
-        pre = left;
-      }
-      if (pre && right) pre.next = right;
-      if (right) {
-        level.push(right);
-        pre = right;
-      }
+    while (level.length) {
+      el = level.pop();
+      el.next = pre;
+      pre = el;
+      let { left, right } = el;
+      right && tmp.unshift(right);
+      left && tmp.unshift(left);
     }
-    if (pre) pre.next = null;
-    nodes = level;
+    level = tmp;
   }
   return root;
 }; // T:O(N) S:O(N/2)
