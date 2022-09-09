1a12f3f375342e22e78ff8d8da64e9910734b1c8
maxgerashenko <mgerashchenko@gmail.com>
2022-09-09 12:10:08 -0400

    2H 4

--- Grokking the Coding Interview/2H_4__hard_NextInterval.ts
+++ Grokking the Coding Interview/2H_4__hard_NextInterval.ts
@@ -2,25 +2,40 @@
 //
 // Next Interval
 
-const find_next_interval = function (intervals) {
-  let result = [];
-  let minStarts = [...intervals.sort((x, y) => x.start - y.start)];
-  let minEnds = [...intervals.sort((x, y) => x.end - y.end)];
-  let mapIndex = new Map();
-  for (let i in intervals) {
-    mapIndex.set(intervals[i], i);
+class Heap {
+  constructor(sort) {
+    this.sort = sort;
+    this.arr = [];
+    this.push = (el) =>
+      (this.arr = [...this.arr, el].sort((a, b) => this.sort(a, b)));
+    this.pop = () => this.arr.shift();
+    this.remove = (toDelete) =>
+      (this.arr = this.arr.filter((el) => el !== toDelete));
   }
-  for (let minEnd of minEnds) {
-    let index = mapIndex.get(minEnd);
-    for (let i in minStarts) {
-      let minStart = minStarts[i];
-      if (minEnd.end > minStart.start) continue;
-      result[index] = mapIndex.get(minStart);
-      break;
+}
+const find_next_interval = function (
+  intervals,
+  results = [],
+  maxEnd = new Heap((x, y) => y.end - x.end),
+  maxStart = new Heap((x, y) => y.start - x.start)
+) {
+  for (let i = 0; i < intervals.length; i++) {
+    let { start, end } = intervals[i];
+    maxEnd.push({ iMaxEnd: i, end });
+    maxStart.push({ iMaxStart: i, start });
+  } // init
+  while (maxEnd.arr.length) {
+    let { end, iMaxEnd } = maxEnd.pop();
+    if (maxStart.arr.length === 0) break;
+    let { start, iMaxStart } = maxStart.pop();
+    while (maxStart.arr.length > 0 && maxStart.arr[0].start >= end) {
+      let el = maxStart.pop();
+      start = el.start;
+      iMaxStart = el.iMaxStart;
     }
-    if (result[index] == null) {
-      result[index] = -1;
-    }
+    maxStart.push({ start, iMaxStart });
+    console.log(start, end);
+    results[iMaxEnd] = start >= end ? [iMaxStart] : -1;
   }
-  return result;
+  return results;
 }; // T:O(NlogN) S:O(N)
