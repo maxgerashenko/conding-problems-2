dc71d35475e1e4b78f6bab2d26d182668585e0f4
maxgerashenko <mgerashchenko@gmail.com>
2022-09-22 10:16:47 -0400

    SS 9

--- Grokking the Coding Interview/SS_9_**_hard_CountOfUniqueBST.ts
+++ Grokking the Coding Interview/SS_9_**_hard_CountOfUniqueBST.ts
@@ -15,13 +15,13 @@ const count_trees = function (end, start = 0, hashMap = new Map(), count = 0) {
 }; // T:O(N 2^N) S:O(N 2^N)
 
 const count_trees = function (n, hashMap = {}, count = 0) {
-  if (!!hashMap[n]) return hashMap[n];
-  if (n <= 1) return 1; // conner case
+  // if(hashMap[n] != null) return hashMap[n];
+  if (n === 0) return 1; // conner case
   for (let i = 1; i <= n; i++) {
-    let leftCount = count_trees(i - 1);
-    let rightCount = count_trees(n - i);
+    let leftCount = count_trees(i - 1, hashMap);
+    let rightCount = count_trees(n - i, hashMap);
     count += leftCount * rightCount;
   }
   hashMap[n] = count;
   return count;
-}; // T:O(N 2^N) S:O(N 2^N)
+}; // T:O(N 2^N) S:O(N 2^N) memoization is T:O(N^2) S:O(N)
