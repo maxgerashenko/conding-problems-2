67378ab1452990387c5e70f7e53314620c8a0f2f
maxgerashenko <mgerashchenko@gmail.com>
2022-09-22 10:16:20 -0400

    SS 9

--- Grokking the Coding Interview/SS_9_**_hard_CountOfUniqueBST.ts
+++ Grokking the Coding Interview/SS_9_**_hard_CountOfUniqueBST.ts
@@ -2,14 +2,26 @@
 //
 // Count of Structurally Unique Binary Search Trees
 
-const count_trees = function (end, start = 1, map = {}, count = 0) {
-  if (map[end - start] != null) return map[end - start]; // memoizetion
-  if (start > end) return 1; // base case
-  for (let i = start; i <= end; i++) {
-    let left = count_trees(i - 1, start, map);
-    let right = count_trees(end, i + 1, map);
-    count += left * right;
+const count_trees = function (end, start = 0, hashMap = new Map(), count = 0) {
+  if (hashMap.has([start, end])) return hashMap.get([start, end]);
+  if (end - start <= 1) return 1; // conner case
+  for (let i = start; i < end; i++) {
+    let leftCount = count_trees(i, start);
+    let rightCount = count_trees(end, i + 1);
+    count += leftCount * rightCount;
   }
-  map[end - start] = count;
+  hashMap.set([start, end], count);
   return count;
-}; // T:O(N2^N) S:O(2^N) memo T:O(2^N) S:O(N)
+}; // T:O(N 2^N) S:O(N 2^N)
+
+const count_trees = function (n, hashMap = {}, count = 0) {
+  if (!!hashMap[n]) return hashMap[n];
+  if (n <= 1) return 1; // conner case
+  for (let i = 1; i <= n; i++) {
+    let leftCount = count_trees(i - 1);
+    let rightCount = count_trees(n - i);
+    count += leftCount * rightCount;
+  }
+  hashMap[n] = count;
+  return count;
+}; // T:O(N 2^N) S:O(N 2^N)
