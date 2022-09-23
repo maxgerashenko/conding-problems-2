25a57c6e8bf8a0ebb3a5cbcbaad4a57d9940314d
maxgerashenko <mgerashchenko@gmail.com>
2022-09-23 12:06:27 -0400

    BS 1

--- Grokking the Coding Interview/BS_1_*_easy_BinarySearch.ts
+++ Grokking the Coding Interview/BS_1_*_easy_BinarySearch.ts
@@ -2,18 +2,25 @@
 //
 // Binary Search
 
-const binary_search = function (arr, key) {
-  let start = 0;
-  let end = arr.length - 1;
-  let isAscending = arr[start] < arr[end];
+const binary_search = function (
+  arr,
+  key,
+  start = 0,
+  end = arr.length - 1,
+  isAscending = arr[start] < arr[end]
+) {
   while (start <= end) {
     let mid = Math.floor(start + (end - start) / 2);
-    if (arr[mid] === key) return arr[mid];
-    if ((isAscending && key > arr[mid]) || (!isAscending && key < arr[mid])) {
-      start = mid + 1;
-      continue;
-    }
-    end = mid - 1;
+    let midEl = arr[mid];
+    if (key === midEl) return midEl;
+    (isAscending && key > midEl) || (!isAscending && key < midEl)
+      ? (start = mid + 1)
+      : (end = mid - 1);
   }
   return -1;
 }; // T:O(logN) S:O(1)
+
+console.log(binary_search([4, 6, 10], 10));
+console.log(binary_search([1, 2, 3, 4, 5, 6, 7], 5));
+console.log(binary_search([10, 6, 4], 10));
+console.log(binary_search([10, 6, 4], 4));
