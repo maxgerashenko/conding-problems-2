e14c6d6a4be097758030c669198fa1f768c08f01
maxgerashenko <mgerashchenko@gmail.com>
2022-09-23 12:06:47 -0400

    BS 2

--- Grokking the Coding Interview/BS_2_medium_CeilingOfANumber.ts
+++ Grokking the Coding Interview/BS_2_medium_CeilingOfANumber.ts
@@ -2,18 +2,22 @@
 //
 // Ceiling of a Number
 
-const search_ceiling_of_a_number = function (arr, key) {
-  let start = 0;
-  let end = arr.length - 1;
+const search_ceiling_of_a_number = function (
+  arr,
+  key,
+  start = 0,
+  end = arr.length - 1
+) {
   if (key > arr[end]) return -1; // conner case
   while (start <= end) {
     let mid = Math.floor(start + (end - start) / 2);
-    if (arr[mid] === key) return arr[mid];
-    if (key > arr[mid]) {
-      start = mid + 1;
+    let midEl = arr[mid];
+    if (key === midEl) return mid; // match
+    if (key < midEl) {
+      end = mid - 1;
       continue;
     }
-    end = mid - 1;
+    start = mid + 1;
   }
-  return arr[start];
+  return start;
 }; // T:O(logN) S:O(1)
