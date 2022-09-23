c762a9343f45649b724b2181a86e0259a2fd79e9
maxgerashenko <mgerashchenko@gmail.com>
2022-09-23 12:25:15 -0400

    BS 3

--- Grokking the Coding Interview/BS_3_medium_NextLetter.ts
+++ Grokking the Coding Interview/BS_3_medium_NextLetter.ts
@@ -2,18 +2,23 @@
 //
 // Next Letter
 
-const search_next_letter = function (letters, key) {
-  let start = 0;
-  let end = letters.length - 1;
-  if (key > letters[end]) return letters[start]; // conner case
+const search_next_letter = function (
+  letters,
+  key,
+  start = 0,
+  end = letters.length - 1
+) {
+  if (key < letters[start]) return letters[end];
+  if (key > letters[end]) return letters[start];
   while (start <= end) {
     let mid = Math.floor(start + (end - start) / 2);
-    if (letters[mid] === key) return letters[mid];
-    if (key < letters[mid]) {
+    let midEl = letters[mid];
+    if (key === midEl) return letters[(mid + 1) % letters.length];
+    if (key < midEl) {
       end = mid - 1;
       continue;
     }
     start = mid + 1;
   }
   return letters[start];
-}; // T:O(logN) S:O(1)
+}; // T:O(LogN) S:O(1)
