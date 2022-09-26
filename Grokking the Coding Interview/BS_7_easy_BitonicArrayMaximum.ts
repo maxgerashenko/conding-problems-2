20e8315b570fc49d789356596dc0053475ef5a2d
maxgerashenko <mgerashchenko@gmail.com>
2022-09-26 11:33:07 -0400

    BS 7

--- Grokking the Coding Interview/BS_7_easy_BitonicArrayMaximum.ts
+++ Grokking the Coding Interview/BS_7_easy_BitonicArrayMaximum.ts
@@ -7,15 +7,15 @@ const find_max_in_bitonic_array = function (
   start = 0,
   end = arr.length - 1
 ) {
-  if (arr[start] > arr[start + 1]) return arr[start]; // conner case
-  if (arr[end] > arr[end - 1]) return arr[end]; // conner case
   while (start <= end) {
+    if (start === end) break;
     let mid = Math.floor(start + end / 2 - start / 2);
-    if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) return arr[mid]; // base case
-    if (arr[mid] < arr[mid + 1]) {
-      start = mid + 1;
+    let midEl = arr[mid];
+    if (midEl > arr[mid + 1]) {
+      end = mid;
       continue;
     }
-    end = mid - 1;
+    start = mid + 1;
   }
+  return start;
 }; // T:O(logN) S:O(1)
