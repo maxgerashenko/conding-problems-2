22d482e655c6acd5daeaa53b65d3498b1eec4806
maxgerashenko <mgerashchenko@gmail.com>
2022-10-05 11:27:12 -0400

    XOR 2

--- Grokking the Coding Interview/XOR_2_**_medium_TwoSingleNumbers.ts
+++ Grokking the Coding Interview/XOR_2_**_medium_TwoSingleNumbers.ts
@@ -6,14 +6,17 @@
 // 2 nums always have 1 bit diff
 // nums of one group will have common diff
 
-function find_single_numbers(nums, xor = 0, diff = 1, first = 0, second = 0) {
-  xor = nums.reduce((pre, el) => pre ^ el, 0);
-  if (xor === 0) return [-1, -1]; // catch error
-  while ((xor & diff) === 0) diff <<= 1;
-
-  for (let el of nums) {
-    first = (el & diff) === 0 ? first ^ el : first;
-    second = (el & diff) === 0 ? second : second ^ el;
+function find_single_numbers(nums, num1 = 0, num2 = 0) {
+  let sum = nums.reduce((pre, cur) => (pre ^= cur), 0);
+  if (sum === 0) return [-1, -1];
+  let diff = 1;
+  while (sum & (diff === 0)) diff <<= 1;
+  for (let num of nums) {
+    if ((num & diff) === 0) {
+      num1 ^= num;
+      continue;
+    }
+    num2 ^= num;
   }
-  return [first, second];
+  return [num1, num2];
 } // T:O(N) S:O(1)
