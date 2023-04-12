9fb518bed3d4ad3e784a380954ead8fe16312d91
maxgerashenko <mgerashchenko@gmail.com>
2023-04-12 17:37:32 -0400

    1 file modified, 1 added

--- prep2023/calculate.ts
+++ prep2023/calculate.ts
@@ -1,162 +1,105 @@
-console.clear();
-(function () {
-  // string
-  // +,*
-  // ( )
-  // no mistake
-  // max length
+//
+//
+//
+//
+//
 
-  // return number
+function isNumber(str) {
+  return /^\d+$/.test(str);
+}
+function excludeNegative(value) {
+  return value === -1 ? Number.MAX_SAFE_INTEGER : value;
+}
+function getFirstSymbolIndex(str) {
+  let indexPlus = excludeNegative(str.indexOf('+'));
+  let indexMinus = excludeNegative(str.indexOf('-'));
+  let indexMulti = excludeNegative(str.indexOf('*'));
 
-  // '5' Number(a)
-  // '1+2'.spitt('+') a + b = c
-  // '1+2' , '1*2' switch +: 'a+b', *; a*b
-  // (1+2) = 3
-  // 1+1+1+1
-
-  // (((1)))
-  /* O(n) */
-  // n = x + x + x +
-  /* O(n) */
-  //(1+2)*3
-  // -?
-
-  /*
-result 48
-{
-  context: '1+32+15'
-  if('(')
-  subContext: {
-    context: '32',
-      subContext:{
-         context: '16*2',
-         subContext:{
-            context: '1+15',
-            if(')')
-          }
-        }
-      }
+  const minIndex = Math.min(indexPlus, indexMinus, indexMulti);
+  return minIndex;
+}
+function calc(left, symbol, right) {
+  let result;
+  switch (symbol) {
+    case '+': {
+      result = Number(left) + Number(right);
+      break;
     }
-
-*/
-
-  /** Key take aways
-   *
-   * all number /^\d+$/
-   * get fist symbol the righ, left
-   * calculate separe functoin
-   * calculate - as factor
-   * calculate numbers but return string
-   * calculates from left <- right, right to left
-   * count ( to find next string in breakets
-   * parse befor '(', between, after ')'
-   * calcString( calcString + parseAll(afterCloseValue))
-   */
-
-  function isNumber(str) {
-    return /^\d+$/.test(str);
+    case '-': {
+      result = Number(left) - Number(right);
+      break;
+    }
+    case '*': {
+      result = Number(left) * Number(right);
+      break;
+    }
+  }
+  return String(result);
+}
+function getProperCloseIndex(str, indexOpen) {
+  let openCount = 1;
+  for (let index = indexOpen + 1; index < str.length; index++) {
+    if (str[index] === '(') {
+      openCount++;
+      continue;
+    }
+    if (str[index] === ')') openCount--;
+    if (openCount === 0) return index;
+    if (openCount < 0) Throw(Error());
+  }
+}
+function parseTocal(str) {
+  if (str === '') return '0';
+  let factor = 1;
+  if (str[0] == '-') {
+    factor = -1;
+    str = str.slice(1);
+  }
+  if (isNumber(str)) {
+    return String(factor * Number(str));
   }
 
-  function getFirstSymbol(str) {
-    const plusIndex = str.indexOf('-');
-    const minusIndex = str.indexOf('+');
-    const multiplyIndex = str.indexOf('*');
+  const minSymbolIndex = getFirstSymbolIndex(str);
+  const symbol = str[minSymbolIndex];
 
-    return Math.min(
-      plusIndex < 0 ? Number.MAX_SAFE_INTEGER : plusIndex,
-      minusIndex < 0 ? Number.MAX_SAFE_INTEGER : minusIndex,
-      multiplyIndex < 0 ? Number.MAX_SAFE_INTEGER : multiplyIndex
-    );
+  const leftPart = str.slice(0, minSymbolIndex);
+  const rightPart = str.slice(minSymbolIndex + 1);
+  const result = calc(
+    String(factor * Number(parseTocal(leftPart))),
+    symbol,
+    parseTocal(rightPart)
+  );
+
+  return String(Number(result));
+}
+
+function parseAll(str) {
+  console.log(str);
+  if (str === '') return '';
+  const indexOpen = str.indexOf('(');
+  const indexClose = str.indexOf(')');
+
+  if (indexClose === -1) return parseTocal(str);
+  if (indexOpen === -1) return '';
+
+  let beforeOpen = '';
+  if (indexOpen > -1) {
+    beforeOpen = str.slice(0, indexOpen);
   }
+  let closeIndex = getProperCloseIndex(str, indexOpen);
+  let between = str.slice(indexOpen + 1, closeIndex);
+  let symbol = str.slice(closeIndex + 1, closeIndex + 2);
+  let afterClose = str.slice(closeIndex + 2);
 
-  const calcStringCache = {};
-  function calcString(str) {
-    if (calcStringCache[str] != null) return calcStringCache[str];
-    let factor = 1;
-    if (str[0] === '-') {
-      factor = -1;
-      str = str.slice(1);
-    }
-    if (isNumber(str)) return String(factor * Number(str));
+  console.log('beforeOpen', beforeOpen);
+  console.log('between', between);
+  console.log('symbol', symbol);
+  console.log('afterClose', afterClose);
 
-    let index = getFirstSymbol(str);
-    let symbol = str[index];
-    const leftString = str.slice(0, index);
-    const rightString = str.slice(index + 1);
+  return (
+    Number(parseTocal(beforeOpen)) +
+    Number(parseAll(parseAll(between) + symbol + parseAll(afterClose)))
+  );
+}
 
-    let result;
-    switch (symbol) {
-      case '+':
-        result =
-          factor * Number(calcString(leftString)) +
-          Number(calcString(rightString));
-        break;
-      case '-':
-        result =
-          factor * Number(calcString(leftString)) -
-          Number(calcString(rightString));
-        break;
-      case '*':
-        result =
-          factor *
-          Number(calcString(leftString)) *
-          Number(calcString(rightString));
-        break;
-    }
-
-    console.log(str, leftString, rightString, result);
-    calcStringCache[str] = String(result);
-    return calcStringCache[str];
-  }
-
-  function getCloseIndex(afterOpen) {
-    let opneCound = 1;
-    let indexClose = 0;
-
-    for (let index = 0; index < afterOpen.length; index++) {
-      if (afterOpen[index] !== '(' && afterOpen[index] !== ')') continue;
-      if (afterOpen[index] === '(') {
-        opneCound++;
-        continue;
-      }
-      opneCound--; // ')'
-
-      if (opneCound === 0) {
-        indexClose = index;
-        break;
-      }
-    }
-
-    return indexClose;
-  }
-
-  const parseCache = {};
-  function parseAll(str) {
-    if (str.length === 0) return '';
-    if (parseCache[str] != null) return parseCache[str];
-    console.log(str);
-    let indexOpen = str.indexOf('(');
-    if (indexOpen === -1) {
-      return Number(calcString(str));
-    }
-
-    const afterOpen = str.slice(indexOpen + 1);
-    const indexClose = indexOpen + 1 + getCloseIndex(afterOpen);
-
-    const beforeOpen = str.slice(0, indexOpen);
-    const between = str.slice(indexOpen + 1, indexClose);
-    const afterClose = str.slice(indexClose + 1);
-    const afterCloseSymbol = afterClose.slice(0, 1);
-    const afterCloseValue = afterClose.slice(1);
-
-    parseCache[str] = calcString(
-      calcString(beforeOpen + parseAll(between)) +
-        afterCloseSymbol +
-        parseAll(afterCloseValue)
-    );
-
-    return parseCache[str];
-  }
-  // 1+(2*3)+3+3+(1+1)
-  console.log(parseAll('-((5+4)*4)+(2*(4+(((2*(1+3))+1)*2)))+2'));
-})();
+console.log(parseAll('1+((2*3)+1-1+(2*4))'));
