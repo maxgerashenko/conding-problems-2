5f23198495c156be4770a1a424944e4a278d1d61
maxgerashenko <mgerashchenko@gmail.com>
2023-04-16 11:08:59 -0400

    1 file modified

--- prep2023/calculate5.ts
+++ prep2023/calculate5.ts
@@ -3,7 +3,7 @@
 
   const isNumber = (el) => /^\d*$/.test(el);
   const isOperator = (el) => /^[*+-]$/.test(el);
-  const getSymbolIndex = (str, start) => str.slice(start).search(/[-+*()]/);
+  const getSymbolIndex = (str) => str.search(/[-+*()]/);
   const calc = (right, left, operator) => left + right;
 
   function calculate(str) {
@@ -14,11 +14,11 @@
     for (let index = 0; index < length; index++) {
       const el = str[index];
       if (isNumber(el)) {
-        let endIndex = index + getSymbolIndex(str, index);
-        console.log(endIndex);
+        let endIndex = index + getSymbolIndex(str.slice(index));
         let num = str.slice(index, endIndex);
         postFix.push(parseInt(num));
-        // index = endIndex;
+        // index = endIndex -1;
+        console.log('num', num);
         console.log('endIndex', endIndex);
         continue;
       }
@@ -28,7 +28,7 @@
     }
     console.log(postFix);
     let result;
-    for (let el of postFix) {
+    while (postFix.length > 0) {
       const right = postFix.pop();
       const left = postFix.pop();
       result += calc(right, left, stack.pop());
