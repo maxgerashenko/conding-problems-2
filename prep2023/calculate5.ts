541208e7d38766d76e866024e37c5cc2c77c1338
maxgerashenko <mgerashchenko@gmail.com>
2023-04-16 11:46:06 -0400

    1 file modified

--- prep2023/calculate5.ts
+++ prep2023/calculate5.ts
@@ -1,11 +1,16 @@
 (() => {
   console.clear();
-  console.log('start');
 
   const isNumber = (el) => /^\d*$/.test(el);
   const isOperator = (el) => /^[*+-]$/.test(el);
   const getSymbolIndex = (str) => str.search(/[-+*()]/);
-  const calc = (right, left, operator) => left + right;
+  const calc = (right, left, operator) => {
+    console.log(parseInt(right));
+    console.log(left);
+    console.log(parseInt(right) + parseInt(left));
+
+    return parseInt(right) + parseInt(left);
+  };
 
   function calculate(str) {
     const stack = [];
@@ -15,28 +20,39 @@
     for (let index = 0; index < length; index++) {
       const el = str[index];
       if (isNumber(el)) {
-        let endIndex = index + getSymbolIndex(str.slice(index));
-        let num = str.slice(index, endIndex);
+        let endIndex = getSymbolIndex(str.slice(index));
+        if (endIndex === -1) {
+          let num = str.slice(index);
+          postFix.push(parseInt(num));
+          break;
+        }
+
+        let num = str.slice(index, index + endIndex);
         postFix.push(parseInt(num));
         index += endIndex - 1;
-        console.log('index', index);
-        console.log('endIndex', endIndex);
         continue;
       }
       if (isOperator(el)) {
         stack.push(el);
+        continue;
       }
     }
     console.log(postFix);
-    let result;
-    while (postFix.length > 0) {
+    console.log(stack);
+    while (postFix.length > 1) {
       const right = postFix.pop();
       const left = postFix.pop();
-      result += calc(right, left, stack.pop());
+
+      console.log('isNumber(right)', isNumber(right));
+      console.log('isNumber(left)', isNumber(left));
+
+      postFix.push(left + right);
+      console.log('isNumber(result)', isNumber(left + right));
+      console.log('result', left + right);
     }
 
-    return result;
+    return postFix[0];
   }
 
-  console.log(calculate('1121+10'));
+  console.log(calculate('11+22+33+44'));
 })();
