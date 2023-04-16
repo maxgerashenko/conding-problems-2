5113ea144f26ef9031763cdfb851a4d71e72bc82
maxgerashenko <mgerashchenko@gmail.com>
2023-04-16 10:51:40 -0400

    1 file modified

---
+++ prep2023/calculate5.ts
@@ -0,0 +1,40 @@
+(() => {
+  console.log('start');
+
+  const isNumber = (el) => /^\d*$/.test(el);
+  const isOperator = (el) => /^[*+-]$/.test(el);
+  const getSymbolIndex = (str, start) => str.slice(start).search(/[-+*()]/);
+  const calc = (right, left, operator) => left + right;
+
+  function calculate(str) {
+    const stack = [];
+    let postFix = [];
+
+    let length = str.length;
+    for (let index = 0; index < length; index++) {
+      const el = str[index];
+      if (isNumber(el)) {
+        let endIndex = index + getSymbolIndex(str, index);
+        let num = str.slice(index, endIndex);
+        postFix.push(parseInt(num));
+        index = endIndex;
+        console.log('endIndex', endIndex);
+        continue;
+      }
+      if (isOperator(el)) {
+        stack.push(el);
+      }
+    }
+    console.log(postFix);
+    let result;
+    for (let el of postFix) {
+      const right = postFix.pop();
+      const left = postFix.pop();
+      result += calc(right, left, stack.pop());
+    }
+
+    return result;
+  }
+
+  console.log(calculate('1121+10'));
+})();
