a94ced8b4f12028d1703fcce28ee83700698b6d9
maxgerashenko <mgerashchenko@gmail.com>
2023-04-16 23:11:28 -0400

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
 // Import stylesheets
 import './style.css';
-import './Blind75/7_Encode and Decode Strings';
+import './Blind75/8_Longest Consecutive Sequence';
+
+var longestConsecutive = function (nums) {
+  let numSet = new Set(nums);
+  let longest = 0;
+  for (let el of numSet) {
+    if (numSet.has(el - 1)) continue;
+    let count = 1;
+    let next = el + 1;
+    while (numSet.has(next)) {
+      count++;
+      next++;
+    }
+    longest = Math.max(longest, count);
+  }
+
+  return longest;
+};
+
+// T:O(n) S:O(n)
