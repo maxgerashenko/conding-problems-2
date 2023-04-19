2be67ca0445b986301e0a309b7be88d448c3829a
maxgerashenko <mgerashchenko@gmail.com>
2023-04-19 12:45:34 -0400

    2 files added

---
+++ Blind75/2P_2_3Sum.ts
@@ -0,0 +1,38 @@
+https://leetcode.com/problems/3sum/description/
+
+const threeSum = function(nums, target = 0) {
+    const results = [];
+    nums.sort((a, b) => a - b); // nums mutation
+
+    for (let first = 0; first < nums.length - 2; first++) {
+        if (first > 0 && nums[first - 1] === nums[first]) continue;
+        let left = first + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            if(left-1 > first && nums[left-1] === nums[left]) {
+                left++;
+                continue
+                };
+            if(nums[right+1] === nums[right]) {
+                right--;
+                continue;
+            };
+            const value = nums[first] + nums[left] + nums[right];
+            if (value === target) {
+                results.push([nums[first], nums[left], nums[right]]);
+                left++;
+                right--;
+                continue;
+            } 
+            if (value < target) {
+                left++;
+                continue;
+            } 
+            right--; // value > target
+        }
+    }
+    return results;
+};
+
+// T:O(nlon * n) O(n^2) S:O(n)
