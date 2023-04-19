8b5c3af8e9bb3d73157b56eff4c466ff6b71e8e6
maxgerashenko <mgerashchenko@gmail.com>
2023-04-19 11:17:59 -0400

    1 file modified Longest Consecutive Sequence

---
+++ Blind75/8_Longest Consecutive Sequence.ts
@@ -0,0 +1,16 @@
+var longestConsecutive = function (nums) {
+  let numSet = new Set(nums);
+  let longest = 0;
+  for (let el of numSet) {
+    if (numSet.has(el - 1)) continue;
+    let count = 1;
+    let next = el + 1;
+    while (numSet.has(next)) {
+      count++;
+      next++;
+    }
+    longest = Math.max(longest, count);
+  }
+
+  return longest;
+};
