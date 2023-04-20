a88455ef9417f869665498952f1da579e74e3d9c
maxgerashenko <mgerashchenko@gmail.com>
2023-04-20 12:33:23 -0400

    2 files added, 1 deleted Sliding Window

---
+++ Blind75/2P_3_Container With Most Water.ts
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/container-with-most-water/submissions/936536679/
+
+// do one thing, focus on hieght
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(heights) {
+    let left = 0;
+    let right = heights.length-1;
+    let maxValue = 0;
+    while(left<right){
+        let minHight = Math.min(heights[left], heights[right]);
+        let value = minHight * (right-left);
+        maxValue = Math.max(maxValue, value);
+        if(heights[left] <=heights[right]){
+            left++;
+            continue;
+        }
+        right--;
+    }
+    return maxValue
+};
