589eaa230f3d9b351af4daa952eb2455b61c1b49
maxgerashenko <mgerashchenko@gmail.com>
2023-04-21 10:34:31 -0400

    1 file modified

--- Blind75/hash_8_Longest Consecutive Sequence.ts
+++ Blind75/hash_8_Longest Consecutive Sequence.ts
@@ -1,3 +1,17 @@
+// https://leetcode.com/problems/longest-consecutive-sequence/
+
+// Easy solution is to sort number in Array
+// then we have [1,2,3,4,100,200]
+// but soring is T:O(nlog) S:O(N)
+
+// To fix it create a set of elements so each key is a number;
+// Go throw the set if el doesn't have previous el it is start
+// the count how many elements exists after the start
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var longestConsecutive = function (nums) {
   let numSet = new Set(nums);
   let longest = 0;
@@ -13,4 +27,4 @@ var longestConsecutive = function (nums) {
   }
 
   return longest;
-};
+}; // T:O(n) S:O(n)
