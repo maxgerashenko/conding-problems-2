dc985d4635d48da5a482fc10a05b2b302c13896e
maxgerashenko <mgerashchenko@gmail.com>
2023-04-27 10:00:13 -0400

    1 file modified

--- Blind75/LL_6_Merge k Sorted Lists.ts
+++ Blind75/LL_6_Merge k Sorted Lists.ts
@@ -4,7 +4,7 @@
 // solution like bread first search
 // and like merge sort algo
 // take every 2 and merge the results
-// complexity
+// Time complexity k long N  S:O(n) AS:O(1)
 
 /**
  * @param {ListNode[]} lists
@@ -45,4 +45,25 @@ var mergeKLists = function (lists) {
   }
 
   return lists[0];
-}; // T:O(klogN) S:O(n) AS:O(1)
+}; // T:O(NlogK) S:O(n) AS:O(1)
+
+var mergeKLists = function (lists) {
+  if (lists == null || lists.length === 0) return null;
+  const minHeap = new MinPriorityQueue({ priority: (x) => x.val });
+
+  // add first elements to heap
+  for (let el of lists) {
+    el && minHeap.enqueue(el);
+  }
+
+  let pre = new ListNode();
+  let cur = pre;
+  while (!minHeap.isEmpty()) {
+    let el = minHeap.dequeue().element;
+    el.next && minHeap.enqueue(el.next);
+    cur.next = el;
+    cur = cur.next;
+  }
+
+  return pre.next;
+}; // T: O(Nlogk) S:O(N + k)
