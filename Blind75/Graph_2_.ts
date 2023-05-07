4acd1c8404430375cf8752c103792bbb5ee7a169
maxgerashenko <mgerashchenko@gmail.com>
2023-05-07 17:52:10 -0400

    2 files added

---
+++ Blind75/Graph_2_.ts
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/clone-graph/
+// Clone Graph
+
+// use DFS in paralell grahs
+// grahs are not trees and could hage cicle
+// use Map to check visited
+
+function cloneGraph(root: Node | null): Node | null {
+    if(!root) return null;
+    let visited = new Map();
+    function dfs(cur){
+        if(visited.has(cur.val)) return visited.get(cur.val);
+        const cur_copy = new Node(cur.val);
+        visited.set(cur.val, cur_copy);
+        for(let node of cur.neighbors)
+            cur_copy.neighbors.push(dfs(node));
+        return cur_copy;
+    } 
+    dfs(root);
+    return visited.get(root.val);
+}; 
+// T:O(N + E) S:O(N^2)
+// call stack N to visit all nodes, and N to store all visited
