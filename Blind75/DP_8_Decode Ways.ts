9f5343b8fe022c6778a632bd70ac740b592a3c06
maxgerashenko <mgerashchenko@gmail.com>
2023-05-21 11:58:34 -0400

    1 file modified

--- Blind75/DP_8_Decode Ways.ts
+++ Blind75/DP_8_Decode Ways.ts
@@ -1,6 +1,33 @@
 // Decode Ways
 // https://leetcode.com/problems/decode-ways/description/
 
+// Make steps 1 or 2
+// + condistion
+// dp reverse
+
+// dp
+function numDecodings(s: string): number {
+  const dp = [];
+  const len = s.length;
+  dp[len] = 1;
+
+  for (let i = len - 1; i >= 0; i--) {
+    if (s[i] == '0') {
+      dp[i] = 0;
+      continue;
+    }
+    dp[i] = dp[i + 1];
+    if (
+      i + 1 < len &&
+      (s[i] === '1' || (s[i] === '2' && Number(s[i + 1]) <= 6))
+    ) {
+      dp[i] += dp[i + 2];
+    }
+  }
+  return dp[0];
+} // T:O(N) S:O(N)
+
+// recursive + cache
 function numDecodings(s: string): number {
   const dp = [];
   const len = s.length;
