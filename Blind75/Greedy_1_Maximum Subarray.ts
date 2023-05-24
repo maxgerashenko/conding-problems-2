08cc4b37a245fc7b3d9064b5f2564a7f364f5948
maxgerashenko <mgerashchenko@gmail.com>
2023-05-24 16:24:29 -0400

    1 file modified

--- Blind75/Greedy_1_Maximum Subarray.ts
+++ Blind75/Greedy_1_Maximum Subarray.ts
@@ -1,2 +1,24 @@
 // Maximum Subarray
 // https://leetcode.com/problems/maximum-subarray/
+
+// DP 0
+// 2 negative values could not be more that 1 negative
+// reset when cur > sum
+// like a window but left border couldbe reset
+
+function maxSubArray(nums: number[]): number {
+  if (nums.length === 0) return null;
+  let curSum = nums[0];
+  let max = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    let el = nums[i];
+    curSum += el;
+    if (curSum < 0 || nums[i] > curSum) {
+      curSum = nums[i];
+    }
+
+    max = Math.max(max, curSum);
+  }
+  return max;
+} // T:O(N) S:O(1)
