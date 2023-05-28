494567be638873908ba0853bf8a4fa67ea188562
maxgerashenko <mgerashchenko@gmail.com>
2023-05-28 12:47:21 -0400

    1 file modified

--- Blind75/Intervals_5_Meeting Rooms II.ts
+++ Blind75/Intervals_5_Meeting Rooms II.ts
@@ -2,23 +2,32 @@
 // https://leetcode.com/problems/meeting-rooms-ii/
 // https://www.lintcode.com/problem/919
 
-minMeetingRooms(intervals: Interval[]): number {
-    let start = intervals.sort((a,b)=>a.start - b.start).map(el => el.start); // T:O(nlogn) S:O(1)
-    let end = intervals.sort((a,b)=>a.end - b.end).map(el => el.end); // T:O(nlogn) S:O(1)
-    let count = 0;
-    let max = 0;
+// 2 arrays, like heap but simpler
+// sort by start and map to start
+// sort by end and map to end
+// iterating all start array, is enought
+// move start when owerlap
+// move end when Not
+// T:O(NlongN) S:O(N)
 
-    let s = 0;
-    let e = 0
-    while(s < start.length){
-      if(start[s] < end[e]) {
-        count++;
-        max = Math.max(max, count);
-        s++;
-        continue;
-      }
-      count--;
-      e++;
+function minMeetingRooms(intervals: Interval[]): number {
+  let start = intervals.sort((a, b) => a.start - b.start).map((el) => el.start); // T:O(nlogn) S:O(1)
+  let end = intervals.sort((a, b) => a.end - b.end).map((el) => el.end); // T:O(nlogn) S:O(1)
+  let count = 0;
+  let max = 0;
+
+  let s = 0;
+  let e = 0;
+  while (s < start.length) {
+    if (start[s] < end[e]) {
+      count++;
+      max = Math.max(max, count);
+      s++;
+      continue;
     }
+    count--;
+    e++;
+  }
 
-    return max;
+  return max;
+} // T:O(NlongN) S:O(N)
