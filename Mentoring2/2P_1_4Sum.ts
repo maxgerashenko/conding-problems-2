60d27e9c2f01730715112e853faa440646fa9b19
maxgerashenko <mgerashchenko@gmail.com>
2023-06-01 17:04:59 -0400

    1 file modified

--- Mentoring2/2P_1_4Sum.ts
+++ Mentoring2/2P_1_4Sum.ts
@@ -1,31 +1,37 @@
-function fourSum(nums: number[], target: number): number[][] {
-  let len = nums.length;
-  if (len < 4) return [];
-  const res = [];
+function fourSum(
+  nums: number[],
+  target: number,
+  len = nums.length
+): number[][] {
+  let res = [];
   nums.sort((a, b) => a - b);
-  for (let l1 = 0; l1 < len; l1++) {
-    // if (l1 > 0 && nums[l1 - 1] === nums[l1]) continue;
-    for (let l2 = l1 + 1; l2 < len; l2++) {
-      let left = l2 + 1;
+
+  for (let L1 = 0; L1 < len - 3; L1++) {
+    for (let L2 = L1 + 1; L2 < len - 2; L2++) {
+      let rest = nums[L1] + nums[L2] - target;
+      let left = L2 + 1;
       let right = len - 1;
-      let sum = l1 + l2;
       while (left < right) {
         let sum2 = nums[left] + nums[right];
-        if (sum + sum2 === target) {
-          res.push([nums[l1], nums[l2], nums[left], nums[right]]);
+        if (sum2 + rest === 0) {
+          console.log([nums[L1], nums[L2], nums[left], nums[right]]);
+          res.push([nums[L1], nums[L2], nums[left], nums[right]]);
           while (left < right && nums[left] === nums[left + 1]) left++;
           while (left < right && nums[right] === nums[right - 1]) right--;
+          console.log([nums[L1], nums[L2], nums[left], nums[right]]);
           left++;
           right--;
           continue;
         }
-        if (sum + sum2 < target) {
+        if (sum2 + rest < 0) {
           left++;
           continue;
         }
         right--;
       }
-      while (nums[l2] === nums[l2 + 1]) l2++;
+      while (nums[L2] === nums[L2 + 1]) L2++;
     }
+    while (nums[L1] === nums[L1 + 1]) L1++;
   }
-}
+  return res;
+} // T:O(N^3) S:O(1), expect O(n)
