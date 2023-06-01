682d6600a3c037d87c3163cc168260dc7e54ddce
maxgerashenko <mgerashchenko@gmail.com>
2023-06-01 19:10:49 -0400

    1 file modified

--- Mentoring2/2P_1_4Sum.ts
+++ Mentoring2/2P_1_4Sum.ts
@@ -1,29 +1,23 @@
-function fourSum(
-  nums: number[],
-  target: number,
-  len = nums.length
-): number[][] {
+function fourSum(nums: number[], target: number): number[][] {
+  let len = nums.length;
   let res = [];
   nums.sort((a, b) => a - b);
 
   for (let L1 = 0; L1 < len - 3; L1++) {
     for (let L2 = L1 + 1; L2 < len - 2; L2++) {
-      let rest = nums[L1] + nums[L2] - target;
       let left = L2 + 1;
       let right = len - 1;
       while (left < right) {
-        let sum2 = nums[left] + nums[right];
-        if (sum2 + rest === 0) {
-          console.log([nums[L1], nums[L2], nums[left], nums[right]]);
+        let sum = nums[L1] + nums[L2] + nums[left] + nums[right];
+
+        if (sum === target) {
           res.push([nums[L1], nums[L2], nums[left], nums[right]]);
           while (left < right && nums[left] === nums[left + 1]) left++;
           while (left < right && nums[right] === nums[right - 1]) right--;
-          console.log([nums[L1], nums[L2], nums[left], nums[right]]);
           left++;
           right--;
-          continue;
         }
-        if (sum2 + rest < 0) {
+        if (sum < target) {
           left++;
           continue;
         }
@@ -34,7 +28,7 @@ function fourSum(
     while (nums[L1] === nums[L1 + 1]) L1++;
   }
   return res;
-} // T:O(N^3) S:O(1), expect O(n^2) for the result
+} // T:O(N^3) S:O(1), except S:O(n^2) result // T:O(N^3) S:O(1), expect O(n^2) for the result
 
 function fourSum(nums: number[], target: number): number[][] {
   let res = [];
