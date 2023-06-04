63a75b87ca3239e9ead3a2801ba33ec9cd3a62bb
maxgerashenko <mgerashchenko@gmail.com>
2023-06-04 18:53:58 -0400

    1 file modified

--- Blind75/Bit_5_Sum of Two Integers.ts
+++ Blind75/Bit_5_Sum of Two Integers.ts
@@ -1,2 +1,11 @@
 // Sum of Two Integers
 // https://leetcode.com/problems/sum-of-two-integers/
+
+function getSum(sum: number, b: number): number {
+  while (b != 0) {
+    let carry = (sum & b) << 1; // if need to carray val to the left
+    sum ^= b; // actual sum 0 ^ 1 || 1 ^ 0
+    b = carry;
+  }
+  return sum;
+} // T:O(1) or n S:O(1)
