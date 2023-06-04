649c7046c4e83382f28c8d3b5d03de0f025f55c2
maxgerashenko <mgerashchenko@gmail.com>
2023-06-04 18:35:13 -0400

    1 file modified

--- Blind75/Bit_4_Missing Number.ts
+++ Blind75/Bit_4_Missing Number.ts
@@ -1,2 +1,28 @@
 // Missing Number
 // https://leetcode.com/problems/missing-number/
+
+// use XOR 2 times
+
+function missingNumber(nums: number[]): number {
+  let n = nums.length;
+  // let res = n*(1+n)/2;
+  let res = 0;
+  for (let i = 1; i < n + 1; i++) {
+    res ^= i;
+  }
+  for (let el of nums) {
+    res ^= el;
+  }
+  return res;
+} // T:O(2n) S:O(1)
+
+// theoretcal sum - curr sum;
+
+// function missingNumber(nums: number[]): number {
+//     let n = nums.length;
+//     let res = n*(1+n)/2; // sum
+//     for(let el of nums) {
+//         res-= el;
+//     }
+//     return res;
+// }; // T:O(n) S:O(1)
