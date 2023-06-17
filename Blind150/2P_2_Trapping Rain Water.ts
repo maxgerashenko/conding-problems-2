845331a4bd8f471cbea7e6dbd369ca1f57211875
maxgerashenko <mgerashchenko@gmail.com>
2023-06-17 13:10:46 -0400

    1 file deleted

--- Blind150/2P_2_Trapping Rain Water.ts
+++ Blind150/2P_2_Trapping Rain Water.ts
@@ -1,2 +1,55 @@
 // Trapping Rain Water
 // https://leetcode.com/problems/trapping-rain-water/
+
+function trap(height: number[]): number {
+  let left = 0;
+  let right = height.length - 1;
+  let leftMax = height[left];
+  let rightMax = height[right];
+  let res = 0;
+
+  while (left < right) {
+    if (height[left] <= height[right]) {
+      leftMax = Math.max(leftMax, height[left]);
+      let val = leftMax - height[left];
+      res += val > 0 ? val : 0;
+      left++;
+      continue;
+    }
+    rightMax = Math.max(rightMax, height[right]);
+    let val = rightMax - height[right];
+    res += val > 0 ? val : 0;
+    right--;
+    continue;
+  }
+
+  return res;
+} // T:O(n) S:O(1)
+
+function trap(height: number[]): number {
+  let mostLef = [0];
+  let mostRigh = [];
+  let len = height.length;
+  mostRigh[len - 1] = 0;
+  let maxLeft = height[0];
+  let maxRight = height[len - 1];
+  let res = 0;
+
+  for (let i = 1; i < len; i++) {
+    maxLeft = Math.max(maxLeft, height[i]);
+    mostLef[i] = maxLeft;
+  }
+
+  for (let i = len - 2; i >= 0; i--) {
+    mostRigh[i] = maxRight;
+    maxRight = Math.max(maxRight, height[i]);
+  }
+
+  for (let i = 0; i < height.length; i++) {
+    console.log(mostLef[i], mostRigh[i], height[i]);
+    let val = Math.min(mostLef[i], mostRigh[i]) - height[i];
+    res += val < 0 ? 0 : val;
+  }
+
+  return res;
+} // T:O(n) S:O(2n)
