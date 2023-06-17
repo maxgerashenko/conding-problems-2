a6b0bd5137f791cdbada83054473417912ec6545
maxgerashenko <mgerashchenko@gmail.com>
2023-06-17 15:10:51 -0400

    1 file modified

--- Blind150/2P_1_Two Sum II.ts
+++ Blind150/2P_1_Two Sum II.ts
@@ -1,6 +1,8 @@
 // Two Sum II
 // https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
+//
+
 function twoSum(numbers: number[], target: number): number[] {
   let left = 0;
   let right = numbers.length - 1;
