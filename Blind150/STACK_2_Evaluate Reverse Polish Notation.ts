872b53329af3f9a12d7c803e015f98a441e5269b
maxgerashenko <mgerashchenko@gmail.com>
2023-06-18 17:17:27 -0400

    1 file modified

--- Blind150/STACK_2_Evaluate Reverse Polish Notation.ts
+++ Blind150/STACK_2_Evaluate Reverse Polish Notation.ts
@@ -1,2 +1,32 @@
 // https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
 // Evaluate Reverse Polish Notation
+
+// use stack
+// ignore operators priority
+// ignore ()
+
+function evalRPN(tokens: string[]): number {
+  let stack = [];
+  let operators = ['+', '-', '*', '/'];
+
+  let operation = {
+    '+': (a, b) => ~~(a + b),
+    '-': (a, b) => ~~(a - b),
+    '*': (a, b) => ~~(a * b),
+    '/': (a, b) => ~~(a / b),
+  };
+  function calc(b, a, operator) {
+    return operation[operator](a, b);
+  }
+
+  for (let el of tokens) {
+    if (operators.indexOf(el) > -1) {
+      let res = calc(stack.pop(), stack.pop(), el);
+      stack.push(res);
+      continue;
+    }
+    stack.push(Number(el));
+  }
+
+  return stack.pop();
+} // T:O(n) S:O(n)
