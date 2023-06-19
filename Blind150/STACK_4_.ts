8502a230a6ecb95ceccf9bfc4c15b1dfe97819e3
maxgerashenko <mgerashchenko@gmail.com>
2023-06-18 21:46:03 -0400

    1 file modified

--- Blind150/STACK_4_.ts
+++ Blind150/STACK_4_.ts
@@ -11,3 +11,27 @@
 // update there index with index diff
 // add bigger value to the stack
 // O:(n) S:O(n)
+
+function dailyTemperatures(temperatures: number[]): number[] {
+  let len = temperatures.length;
+  let res = Array(len).fill(0);
+  let stack = [];
+
+  for (let i = 0; i < len; i++) {
+    let el = temperatures[i];
+    while (stack.slice(-1)[0]?.val < el) {
+      let { index } = stack.pop();
+      res[index] = i - index;
+    }
+    stack.push({ index: i, val: el });
+  }
+
+  let count = 0;
+  while (stack.length > 0) {
+    stack.pop();
+    res[len - 1 - count] = 0;
+    count++;
+  }
+
+  return res;
+} // T:O(N) S:O(N)
