6b70e167ba3e590a25d42fa32ba191ea93eee1c7
maxgerashenko <mgerashchenko@gmail.com>
2023-06-18 23:23:00 -0400

    1 file modified

--- Blind150/STACK_5_Car Fleet.ts
+++ Blind150/STACK_5_Car Fleet.ts
@@ -1,2 +1,25 @@
 // https://leetcode.com/problems/car-fleet/
 // Car Fleet
+
+// revers order
+// check closest first
+// if cur < last it is a fleet
+// if cur > last it is a new fleet
+
+function carFleet(target: number, position: number[], speed: number[]): number {
+  let carPoop = position
+    .map((el, i) => ({ pos: el, speed: speed[i] }))
+    .sort((a, b) => b.pos - a.pos);
+  let stack = [];
+  for (let { pos, speed } of carPoop) {
+    let cur = target / speed - pos / speed;
+    if (stack.length === 0) {
+      stack.push(cur);
+      continue;
+    }
+    if (cur <= stack.slice(-1)[0]) continue;
+    stack.push(cur);
+  }
+
+  return stack.length;
+} // T:O(nlogn) S:O(n)
