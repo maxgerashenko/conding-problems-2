00b5517ad723d336b1f1f2bfe162336fe7196c16
maxgerashenko <mgerashchenko@gmail.com>
2023-07-13 00:17:36 -0400

    1 file modified

--- Blind150/TREE_1_Diameter of Binary Tree.ts
+++ Blind150/TREE_1_Diameter of Binary Tree.ts
@@ -1,2 +1,29 @@
-//
+// https://leetcode.com/problems/diameter-of-binary-tree/description/
 // Diameter of Binary Tree
+
+// Use DFS
+// call max
+// retrun diffrent
+// use -1 to mark leaf node
+// compensate with +2 for max
+// return 1 + Math.max(left, right)
+// T:O(N) S:O(1)
+
+function diameterOfBinaryTree(root: TreeNode | null): number {
+  let max = 0;
+
+  function dfs(node: TreeNode) {
+    if (node == null) return -1;
+
+    let left = dfs(node.left);
+    let right = dfs(node.right);
+
+    max = Math.max(max, left + right + 2);
+
+    return 1 + Math.max(left, right);
+  }
+  dfs(root);
+
+  return max;
+} // T:O(N) S:O(1)
+'
