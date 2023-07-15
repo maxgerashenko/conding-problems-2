16388aeffb14e14afce6e6510deed6cfa5a1494d
maxgerashenko <mgerashchenko@gmail.com>
2023-07-15 15:28:13 -0400

    1 file added

---
+++ Blind150/HA_3_K Closest Points to Origin.ts
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/k-closest-points-to-origin/description/
+// K Closest Points to Origin
+
+function kClosest(points: number[][], k: number): number[][] {
+  let maxHeap = new Heap((a, b) => b.val - a.val);
+  let [x, y] = points.pop();
+  maxHeap.add({ coord: [x, y], val: Math.pow(x, 2) + Math.pow(y, 2) });
+
+  for (let [x, y] of points) {
+    let val = Math.pow(x, 2) + Math.pow(y, 2);
+    if (maxHeap.heap.length < k) {
+      maxHeap.add({ coord: [x, y], val });
+      continue;
+    }
+    if (val > maxHeap.peek().val) continue;
+    maxHeap.remove();
+    maxHeap.add({ coord: [x, y], val });
+  }
+
+  return maxHeap.heap.map((x) => x.coord);
+} // T:O(NlogK) S:O(K)
+
