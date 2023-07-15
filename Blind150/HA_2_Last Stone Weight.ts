a4f1f700851b74269c9232351d07f503382ad8a5
maxgerashenko <mgerashchenko@gmail.com>
2023-07-15 14:45:50 -0400

    1 file modified

--- Blind150/HA_2_Last Stone Weight.ts
+++ Blind150/HA_2_Last Stone Weight.ts
@@ -1,2 +1,19 @@
 // https://leetcode.com/problems/last-stone-weight/
 // Last Stone Weight
+
+// Use heap
+// add all
+// take two and smash
+// put the rest back
+// repeat until 1 left
+// T:O(nLogN) S:O(n)
+
+function lastStoneWeight(stones: number[]): number {
+  let maxHeap = new Heap((a, b) => b - a);
+  for (let el of stones) maxHeap.add(el);
+  while (maxHeap.heap.length > 1) {
+    let val = maxHeap.remove() - maxHeap.remove();
+    maxHeap.add(val);
+  }
+  return maxHeap.peek();
+} // T:O(nlogn) S:O(n)
