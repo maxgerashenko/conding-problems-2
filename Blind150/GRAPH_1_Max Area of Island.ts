21776586d3c38ad087e251edc568769ced60e678
maxgerashenko <mgerashchenko@gmail.com>
2023-07-20 20:13:16 -0400

    1 file modified

--- Blind150/GRAPH_1_Max Area of Island.ts
+++ Blind150/GRAPH_1_Max Area of Island.ts
@@ -1,2 +1,30 @@
 // https://leetcode.com/problems/max-area-of-island/
 // GRAPH_1_Max Area of Island.ts
+
+// Use DFS
+// use j,j
+// conner case 0 or i<0 || i===n
+// return 1 + sum for directions
+// mark visited cells
+// use for for to try to start from every cell
+
+function maxAreaOfIsland(grid: number[][]): number {
+  let max = 0;
+  let m = grid.length;
+  let n = grid[0].length;
+
+  function dfs(j, i) {
+    if (i < 0 || i === n || j < 0 || j === m || grid[j][i] === 0) return 0; // base case
+    grid[j][i] = 0; // mark as visited
+
+    let local = dfs(j, i + 1) + dfs(j, i - 1) + dfs(j + 1, i) + dfs(j - 1, i);
+    return 1 + local;
+  }
+  for (let j = 0; j < m; j++) {
+    for (let i = 0; i < n; i++) {
+      max = Math.max(max, dfs(j, i));
+    }
+  }
+
+  return max;
+} // T:O(m*n) S:O(m*n)
