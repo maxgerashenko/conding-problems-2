0e1bd7a67fa36047a461a83a7279a9e0ec8d6690
maxgerashenko <mgerashchenko@gmail.com>
2023-07-25 08:21:49 -0400

    1 file modified

--- Blind150/Mistakes.ts
+++ Blind150/Mistakes.ts
@@ -1,1 +1,5 @@
 // push([x,y]); let {x,y} = ..pop();
+// push([x,y]); let [x,y] = ..pop();
+
+// new Array(n).fill(new Array(n).fill(false))
+// let visited = new Array(n).fill(0).map(() => new Array(n).fill(false));
