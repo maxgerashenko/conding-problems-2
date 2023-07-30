ed60319891ad65a0227c19da4d8269393adc8441
maxgerashenko <mgerashchenko@gmail.com>
2023-07-30 17:24:12 -0400

    1 file modified

--- Blind150/DP_D2_2_Coin Change II.ts
+++ Blind150/DP_D2_2_Coin Change II.ts
@@ -28,3 +28,35 @@ function change(amount: number, coins: number[]): number {
 
   return dfs(0, amount);
 } // T:O(m*n) S:O(m*n)
+
+// DP amount vs coins, not the best space complexity
+//
+// Use dp
+// matrix
+// always iterate i reversed
+// it is bottom up
+// return is dp[amount][0], I know WTF
+// matrix is amount as m, coins as n
+// amount [0] = 1 1 1 1 1 as 5 - 5 =0 === 1 option
+// ignore result < 0 index
+// add 0 row with 1s for amoutn [amount - coin[j]][]
+// add last row with 0s for couins,  [][i+1]
+// m = anount
+//
+// T:O(m*n) S:O(m*n)
+function change(amount: number, coins: number[]): number {
+  let m = amount + 1;
+  let n = coins.length + 1;
+  let dp = Array(m).fill(0);
+  dp = dp.map((el) => Array(n).fill(0));
+  dp[0] = dp[0].map((el) => 1);
+
+  for (let j = 1; j < m; j++)
+    for (let i = n - 2; i >= 0; i--) {
+      dp[j][i] = dp[j][i + 1];
+      if (j - coins[i] < 0) continue; // conner case
+      dp[j][i] += dp[j - coins[i]][i]; // base case
+    }
+
+  return dp[amount][0];
+} // T:O(m*n) S:O(m*n)
