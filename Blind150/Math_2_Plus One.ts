c93237592853a40b00fd0a5c3090a6507877c6ad
maxgerashenko <mgerashchenko@gmail.com>
2023-08-06 17:38:40 -0400

    1 file modified

--- Blind150/Math_2_Plus One.ts
+++ Blind150/Math_2_Plus One.ts
@@ -1,2 +1,30 @@
 // https://leetcode.com/problems/plus-one/
 // Plus One
+
+// use array
+// use exclude logic
+// start from the end
+// if el < 9 it is the end el++;
+// if 9 then 0
+// if first element add 1 to the begining
+//
+// T:O(n) S:O(1)
+
+function plusOne(digits: number[]): number[] {
+  let n = digits.length;
+  let index = n - 1;
+
+  while (index > -1) {
+    if (digits[index] < 9) {
+      digits[index]++;
+      break;
+    }
+    digits[index] = 0;
+    if (index === 0) {
+      digits.unshift(1);
+    }
+    index--;
+  }
+
+  return digits;
+} // T:O(n) S:O(1)
