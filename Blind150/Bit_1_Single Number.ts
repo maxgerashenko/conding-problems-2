760c898980838f37219e685d779a749b423cdb44
maxgerashenko <mgerashchenko@gmail.com>
2023-08-09 10:24:15 -0400

    1 file modified

--- Blind150/Bit_1_Single Number.ts
+++ Blind150/Bit_1_Single Number.ts
@@ -1,3 +1,16 @@
 // https://leetcode.com/problems/single-number/
 // Single Number
 
+// use ^=
+// applyed twice cancel the number
+// something that appears ones will stays
+// T:O(n) S:O(1)
+
+var singleNumber = function (nums) {
+  let res = 0;
+  for (let num of nums) {
+    res ^= num;
+  }
+
+  return res;
+}; // T:O(n) S:O(n)
