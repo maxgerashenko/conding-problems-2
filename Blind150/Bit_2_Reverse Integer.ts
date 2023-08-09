bb440fa25129471bffcdc842a27d4023b721523d
maxgerashenko <mgerashchenko@gmail.com>
2023-08-09 11:04:23 -0400

    1 file modified

--- Blind150/Bit_2_Reverse Integer.ts
+++ Blind150/Bit_2_Reverse Integer.ts
@@ -1,2 +1,22 @@
 // https://leetcode.com/problems/reverse-integer/
 // Reverse Integer
+
+// use Math.pow(2,31)
+// use x%10
+// use ~~(x/10) integer devision
+// use res = res*10 + value % 10
+// T:O(1) S:O(1)
+
+var reverse = function (value) {
+  let MIN = -1 * Math.pow(2, 31);
+  let MAX = Math.pow(2, 31) - 1;
+  let res = 0;
+
+  while (value) {
+    let tmp = value % 10;
+    value = ~~(value / 10);
+    res = res * 10 + tmp;
+  }
+
+  return res < MIN || res > MAX ? 0 : res;
+}; // T:O(1) S:O(1)
