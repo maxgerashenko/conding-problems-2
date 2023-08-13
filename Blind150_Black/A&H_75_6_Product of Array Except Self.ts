2a39a91186f4ff8ad80197298ac8f6efacdc384b
maxgerashenko <mgerashchenko@gmail.com>
2023-08-13 14:24:51 -0400

    1 file modified

--- Blind150_Black/A&H_75_6_Product of Array Except Self.ts
+++ Blind150_Black/A&H_75_6_Product of Array Except Self.ts
@@ -1,2 +1,27 @@
 // https://leetcode.com/problems/product-of-array-except-self/
 // Product of Array Except Self
+
+// can’t use division
+// iterate pre-product
+// iterate post product
+// return res
+// T:O(n) S:O(1)
+
+function productExceptSelf(nums: number[]): number[] {
+  let res = [];
+  let n = nums.length;
+
+  let pre = 1;
+  for (let el of nums) {
+    res.push(pre);
+    pre *= el;
+  }
+
+  let post = 1;
+  for (let index = n - 1; index >= 0; index--) {
+    res[index] *= post;
+    post *= nums[index];
+  }
+
+  return res;
+} // T:O(n) S:O(n)
