2ba09acab9b77bd47f7db6ae8d410c89c5e269aa
maxgerashenko <mgerashchenko@gmail.com>
2023-08-12 20:05:24 -0400

    1 file modified

--- Blind150/SW_2_ Sliding Window Maximum.ts
+++ Blind150/SW_2_ Sliding Window Maximum.ts
@@ -10,30 +10,23 @@
 // increase right;
 
 function maxSlidingWindow(nums: number[], k: number): number[] {
-  let maxQueue = [];
+  let n = nums.length;
+  let queueIndex = []; // queue structure
   let res = [];
-  let right = 0;
-  let left = 0;
 
-  while (right < nums.length) {
-    // don't add new element if prev is smaller, remove them
-    while (maxQueue.length > 0 && nums[maxQueue.slice(-1)[0]] <= nums[right]) {
-      maxQueue.pop();
-    }
-    maxQueue.push(right);
+  for (let index = 0; index < n; index++) {
+    while (
+      queueIndex.length &&
+      nums[queueIndex[queueIndex.length - 1]] < nums[index]
+    )
+      queueIndex.pop();
+    queueIndex.push(index);
 
-    if (left > maxQueue[0]) {
-      maxQueue.shift();
-    }
+    if (index < k - 1) continue;
 
-    // k4 === index3
-    if (right + 1 >= k) {
-      // add and remove current max
-      res.push(nums[maxQueue[0]]);
-      left++;
-    }
-    right++;
+    if (queueIndex[0] <= index - k) queueIndex.shift();
+    res.push(nums[queueIndex[0]]);
   }
 
   return res;
-} // T:O(N) S:O(N)
+} // T:O(n) S:O(k)
