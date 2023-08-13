88359d0a4866a014f0186db42723f509b321ae43
maxgerashenko <mgerashchenko@gmail.com>
2023-08-13 14:05:18 -0400

    1 file modified

--- Blind150_Black/A&H_75_5_Top K Frequent Elements.ts
+++ Blind150_Black/A&H_75_5_Top K Frequent Elements.ts
@@ -1,8 +1,35 @@
 // https://leetcode.com/problems/top-k-frequent-elements/description/
 // Top K Frequent Elements
 
+function topKFrequent(nums: number[], k: number): number[] {
+  let hasMapCount = {};
+  let res = [];
+  let bucketList = [];
+  let n = nums.length;
 
+  for (let el of nums) {
+    hasMapCount[el] = 1 + (hasMapCount[el] || 0);
+  }
 
+  for (let key of Object.keys(hasMapCount)) {
+    bucketList[hasMapCount[key]] = [
+      ...(bucketList[hasMapCount[key]] || []),
+      key,
+    ];
+  }
+
+  let index = n;
+  while (res.length < k && index >= 0) {
+    if (!bucketList[index] || !bucketList[index].length) {
+      index--;
+      continue;
+    }
+
+    res.push(bucketList[index].pop());
+  }
+
+  return res;
+} // T:O(n) S:O(n)
 
 function topKFrequent(nums: number[], k: number): number[] {
   let maxHeap = new Heap((a, b) => b.count - a.count);
