e4523a8ab3220640e8caa71409747b153cec9f15
maxgerashenko <mgerashchenko@gmail.com>
2023-08-17 20:21:10 -0400

    1 file modified

---
+++ Blind150_Black/2P_150_2_Trapping Rain Water.ts
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/trapping-rain-water/
+// Trapping Rain Water
+
+// use 2P
+// start from far left and far right
+// set maxLeft and maxRight
+// ignore fist interation
+// track maxLeft and maxRight
+// udpate left++ and right-- when the smallest
+// leave maxL or maxR but move left and right
+// calculate count for left or right based on max-cur
+// udpate max first to avoid negative numbers
+
+function trap(height: number[]): number {
+  let n = height.length;
+  let left = 0;
+  let right = n - 1;
+  let count = 0;
+  let leftMax = height[0];
+  let rightMax = height[right];
+
+  while (left < right) {
+    if (leftMax <= rightMax) {
+      left++;
+      leftMax = Math.max(leftMax, height[left]);
+      count += leftMax - height[left];
+      continue;
+    }
+
+    right--;
+    rightMax = Math.max(rightMax, height[right]);
+    count += rightMax - height[right];
+  }
+
+  return count;
+} //T:O(n) S:O(1)
