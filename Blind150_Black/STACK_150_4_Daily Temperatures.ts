3af29d38472c21107b7e04dcb473eba378a00f85
maxgerashenko <mgerashchenko@gmail.com>
2023-08-18 18:53:08 -0400

    1 file modified

---
+++ Blind150_Black/STACK_150_4_Daily Temperatures.ts
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/daily-temperatures/
+// Daily Temperatures
+
+// use stack
+// keep stack monotonicaly decreasing
+// set res by index
+
+var dailyTemperatures = function (temps) {
+  let monotoIndexStack = [];
+  let res = [];
+  let n = temps.length;
+
+  for (let index = 0; index < n; index++) {
+    let el = temps[index];
+    while (el > temps[monotoIndexStack[monotoIndexStack.length - 1]]) {
+      let stackIndex = monotoIndexStack.pop();
+      res[stackIndex] = index - stackIndex;
+    } // keep monotolicaly and set values
+    monotoIndexStack.push(index);
+  } // feed the stack
+
+  for (let index of monotoIndexStack) {
+    res[index] = 0;
+  } // add 0's
+  return res;
+}; //T:O(n) S:O(n)
