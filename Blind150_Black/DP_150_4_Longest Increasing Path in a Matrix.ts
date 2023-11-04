a722f0f13ec2b75dfdac4d81882ff37c5505b2f7
maxgerashenko <mgerashchenko@gmail.com>
2023-11-04 15:20:21 -0400

    1 file modified

--- Blind150_Black/DP_150_4_Longest Increasing Path in a Matrix.ts
+++ Blind150_Black/DP_150_4_Longest Increasing Path in a Matrix.ts
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/longest-palindromic-substring/description/
+// https://leetcode.com/problems/longest-palindromic-substring/description
 // Longest Palindromic Substring
 
 // DP
