17f1367d3e25e7de26a65dc9926fc83a6a4d845c
maxgerashenko <mgerashchenko@gmail.com>
2024-01-07 18:09:12 -0500

    1 file modified

--- Blind150_Black/GRAPH_150_3_Rotting Oranges.ts
+++ Blind150_Black/GRAPH_150_3_Rotting Oranges.ts
@@ -9,51 +9,56 @@
 // bfs && level > 0
 // return count ===0  ? rounds : -1;
 
-let orangesRotting = function (grid) {
-  let rounds = 0;
-  let count = 0;
-  let level = [];
-  let m = grid.length;
-  let n = grid[0].length;
-  let dir = [
+// conner cases
+// 2 only // 0
+// 1 only // -1
+// start from new roatted
+
+function orangesRotting(grid: number[][]): number {
+  const rowsLen = grid.length;
+  const colsLen = grid[0].length;
+  const dir = [
     [1, 0],
     [-1, 0],
     [0, 1],
     [0, -1],
   ];
+  let newRotten = [];
+  let count = -1;
+  let totalCount = 0;
 
-  // init
-  for (let j = 0; j < m; j++) {
-    for (i = 0; i < n; i++) {
-      if (grid[j][i] == 2) level.push([j, i]);
-      if (grid[j][i] == 1) count++;
-    }
-  }
+  for (let j = 0; j < rowsLen; j++)
+    for (let i = 0; i < colsLen; i++) {
+      let el = grid[j][i];
+      if (el === 1) totalCount++;
+      if (el !== 2) continue;
+      newRotten.push([j, i]);
+    } // init
+
+  if (newRotten.length === 0) return totalCount === 0 ? 0 : -1;
+  count++;
+
   function bfs() {
-    let curent = [];
-    for (let [j, i] of level) {
+    let tmp = [];
+
+    for (let [j, i] of newRotten) {
       for (let [dj, di] of dir) {
-        let newj = j + dj;
-        let newi = i + di;
-        if (
-          grid[newj] == null ||
-          grid[newj][newi] == null ||
-          grid[newj][newi] == 0 ||
-          grid[newj][newi] == 2
-        )
+        let [newJ, newI] = [j + dj, i + di];
+        if (newJ < 0 || newJ >= rowsLen || newI < 0 || newI >= colsLen)
           continue;
-        grid[newj][newi] = 2;
-        count--;
-        curent.push([newj, newi]); // base case
+        if (grid[newJ][newI] !== 1) continue;
+        grid[newJ][newI] = 2;
+        totalCount--;
+        tmp.push([newJ, newI]);
       }
     }
-    level = [...curent];
 
-    if (level.length > 0) {
-      rounds++;
-      bfs();
-    }
+    newRotten = [...tmp];
+    if (newRotten.length === 0) return;
+    count++;
+    bfs();
   }
   bfs();
-  return count === 0 ? rounds : -1;
-}; // T:O(NM) S:O(MN)
+
+  return totalCount === 0 ? count : -1;
+} // T:O(m*n) S:O(m*n)
