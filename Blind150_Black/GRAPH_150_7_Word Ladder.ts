29d4d9c297fa92654c302055fba54a876df0073f
maxgerashenko <mgerashchenko@gmail.com>
2024-01-23 22:32:14 -0500

    1 file modified

--- Blind150_Black/GRAPH_150_7_Word Ladder.ts
+++ Blind150_Black/GRAPH_150_7_Word Ladder.ts
@@ -7,49 +7,43 @@
 // level []
 // T:O(N*M*26) S:O(N)
 
-function ladderLength(
-  beginWord: string,
-  endWord: string,
-  wordList: string[]
-): number {
-  const visited = new Set();
-  const adjList = {};
-  let level = [beginWord];
-  let step = 1;
-  for (let word of wordList) {
+function ladderLength(beginWord, endWord, wordList) {
+  let masksMap = {};
+  if (wordList.indexOf(endWord) == -1) return 0;
+  const getMask = (word, i) =>
+    `${word.substring(0, i)}*${word.substring(i + 1)}`;
+
+  // Create masks
+  for (let word of wordList)
     for (let i = 0; i < word.length; i++) {
-      let tmp = word.split('');
-      tmp[i] = '*';
-      let mask = tmp.join('');
-      if (adjList[mask] == null) {
-        adjList[mask] = [];
+      let mask = getMask(word, i);
+      if (!masksMap[mask]) {
+        masksMap[mask] = [];
       }
-      adjList[mask].push(word);
-    } // set adj list
-  }
+      masksMap[mask].push(word);
+    } // init T:O(n*m*m)
 
-  function bfs() {
+  let queue = [beginWord];
+  const visited = new Set(queue);
+  let levelCount = 1;
+
+  while (queue.length) {
     let tmp = [];
-
-    for (let word of level) {
-      if (visited.has(word)) continue;
-      if (word === endWord) return true;
-      visited.add(word);
+    for (let word of queue) {
+      if (word === endWord) return levelCount;
       for (let i = 0; i < word.length; i++) {
-        let arr = word.split('');
-        arr[i] = '*';
-        let mask = arr.join('');
-
-        if (adjList[mask] == null) continue;
-        tmp.push(...adjList[mask]);
+        let mask = getMask(word, i);
+        if (masksMap[mask] == null) continue;
+        for (let el of masksMap[mask]) {
+          if (visited.has(el)) continue;
+          visited.add(el);
+          tmp.push(el);
+        }
       }
     }
+    queue = tmp;
+    levelCount++;
+  } // bfs
 
-    level = tmp;
-    if (level.length === 0) return;
-    step++;
-    if (bfs()) return true;
-  }
-
-  return bfs() ? step : 0;
-} // T:O(N * Length * 26) S:O(N)// T:O(N * Length * 26) S:O(N)
+  return 0;
+} // T:O(n*m^2) S:O(n)
