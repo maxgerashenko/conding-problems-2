152ec789cd9065e3a2607e487e017ea221a0f730
maxgerashenko <mgerashchenko@gmail.com>
2024-06-07 00:21:01 -0400

    1 file modified

--- Blind150_BlackHard/DYN_75_1_Climbing Stairs.ts
+++ Blind150_BlackHard/DYN_75_1_Climbing Stairs.ts
@@ -4,13 +4,14 @@
 // Like fibonachi
 
 function climbStairs(n: number): number {
-    if (n < 3) return n;
-    let cur = 2;
-    let pre = 1;
-    for (let i = 2; i < n; i++) {
-        let tmp = cur;
-        cur += pre;
-        pre = tmp;
-    }
-    return cur;
-}; // T:O(n) S:(1);
+  let pre = 0;
+  let cur = 1;
+
+  for (let i = 0; i < n; i++) {
+    let tmp = cur;
+    cur = pre + cur;
+    pre = tmp;
+  }
+
+  return cur;
+} // T:O(n) S:O(1)
