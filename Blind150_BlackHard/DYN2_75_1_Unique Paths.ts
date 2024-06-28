bcc8c363ef7be6359fd98d7bdd83313928be7256
maxgerashenko <mgerashchenko@gmail.com>
2024-06-28 02:27:08 -0400

    1 file modified

--- Blind150_BlackHard/DYN2_75_1_Unique Paths.ts
+++ Blind150_BlackHard/DYN2_75_1_Unique Paths.ts
@@ -1,3 +1,19 @@
 // Unique Paths
 //
 // https://leetcode.com/problems/unique-paths/description/
+
+function uniquePaths(m: number, n: number): number {
+  if (m < 2) return 1;
+
+  let row = Array(n).fill(1);
+  for (let j = 1; j < m; j++) {
+    let tmp = Array(n);
+    tmp[0] = 1;
+    for (let i = 1; i < n; i++) {
+      tmp[i] = row[i] + tmp[i - 1];
+    }
+    row = tmp;
+  }
+
+  return row[n - 1];
+} // T:O(n*m) S:O(2n)
