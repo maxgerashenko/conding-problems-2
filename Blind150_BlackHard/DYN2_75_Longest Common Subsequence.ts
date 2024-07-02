83cb600de48957764bef52f7f230b932ae683530
maxgerashenko <mgerashchenko@gmail.com>
2024-07-02 01:15:53 -0400

    1 file modified

--- Blind150_BlackHard/DYN2_75_Longest Common Subsequence.ts
+++ Blind150_BlackHard/DYN2_75_Longest Common Subsequence.ts
@@ -3,21 +3,20 @@
 // https://leetcode.com/problems/longest-common-subsequence/description/
 
 function longestCommonSubsequence(text1: string, text2: string): number {
-    let n = text1.length;
-    let m = text2.length;
-    let dp = Array(m + 1).fill(Array(n + 1).fill(0));
+  let n = text1.length;
+  let m = text2.length;
+  let dp = Array(m + 1)
+    .fill(null)
+    .map((row) => Array(n + 1).fill(0));
 
-    for (let j = m - 1; j >= 0; j--) {
-        for (let i = n - 1; i >= 0; i--) {
-            if (text1[i] === text2[j]) {
-                dp[j][i] = dp[j + 1][i + 1] + 1;
-                continue;
-            } else {
-
-            dp[j][i] = Math.max(dp[j + 1][i], dp[j][i + 1]);
-            }
-        }
+  for (let j = m - 1; j >= 0; j--) {
+    for (let i = n - 1; i >= 0; i--) {
+      dp[j][i] =
+        text1[i] === text2[j]
+          ? dp[j + 1][i + 1] + 1
+          : Math.max(dp[j + 1][i], dp[j][i + 1]);
     }
+  }
 
-    return dp[0][0];
-}; // T:O(n*m) S:O(n*m);
+  return dp[0][0];
+} // T:O(n*m) S:O(n*m);
