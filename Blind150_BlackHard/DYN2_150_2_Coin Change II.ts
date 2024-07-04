e3ba08ff61b7249c411b2e6d6378d77903978459
maxgerashenko <mgerashchenko@gmail.com>
2024-07-04 02:23:31 -0400

    1 file modified

--- Blind150_BlackHard/DYN2_150_2_Coin Change II.ts
+++ Blind150_BlackHard/DYN2_150_2_Coin Change II.ts
@@ -1,3 +1,17 @@
 // Coin Change II
 //
 // https://leetcode.com/problems/coin-change-ii/description/
+
+function change(amount: number, coins: number[]): number {
+  let dp = Array(amount + 1).fill(0);
+  dp[0] = 1;
+
+  for (let coin of coins) {
+    let pre = [...dp];
+    for (let i = 1; i < amount + 1; i++) {
+      dp[i] = (i - coin < 0 ? 0 : dp[i - coin]) + pre[i];
+    }
+  }
+
+  return dp[amount];
+} // T:O(n*m) S:O(n)
