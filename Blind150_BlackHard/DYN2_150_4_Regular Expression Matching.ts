12d17dc4b823e566a8fe1dc3a7c48030b2acbfa0
maxgerashenko <mgerashchenko@gmail.com>
2024-07-09 01:48:54 -0400

    1 file modified

--- Blind150_BlackHard/DYN2_150_4_Regular Expression Matching.ts
+++ Blind150_BlackHard/DYN2_150_4_Regular Expression Matching.ts
@@ -3,32 +3,33 @@
 // https://leetcode.com/problems/regular-expression-matching/description/
 
 // critical case when i is at the end but patern is not yet but should be true
-function isMatch(str: string, pat: string): boolean {
-    let n = str.length;
-    let m = pat.length;
-    let dp = {};
+function isMatch(s: string, p: string): boolean {
+  let n = s.length;
+  let m = p.length;
+  let dp = {};
 
-    function dfs(i, j) {
-        let key = i + ', ' + j;
-        if (key in dp) return dp[key];
+  function dfs(i = 0, j = 0) {
+    let key = i + ',' + j;
+    if (key in dp) return dp[key]; // cach
 
-        if (i >= n && j >= m) return true;
-        if (j >= m) return false;
+    if (i >= n && j >= m) return true; // base case - all good
+    if (j >= m) return false; // base case - not match in pattern
 
-        let isMatch = i < n && (str[i] === pat[j] || pat[j] === '.');
-        if (j + 1 < m && pat[j + 1] === '*') {
-            if (isMatch) {
-                dp[key] = dfs(i, j + 2) || dfs(i + 1, j);
-                return dp[key];
-            }
-            dp[key] = dfs(i, j + 2);
-            return dp[key];
-        }
-        if (isMatch) {
-            dp[key] = dfs(i + 1, j + 1);
-            return dp[key];
-        }
-        return false;
+    let isMatch = s[i] === p[j] || p[j] === '.';
+    if (p[j + 1] === '*') {
+      if (i < n && isMatch) {
+        dp[key] = dfs(i + 1, j) || dfs(i, j + 2); // base case
+        return dp[key];
+      }
+      dp[key] = dfs(i, j + 2); // conner case - ignore * and finish pattern
+      return dp[key];
     }
-    return dfs(0, 0);
-}; // T:O(max(n,m)) S:O(n)
+    if (i < n && isMatch) {
+      dp[key] = dfs(i + 1, j + 1); // base case
+      return dp[key];
+    }
+    return false;
+  }
+
+  return dfs();
+} // T:O(N) S:O(N)
