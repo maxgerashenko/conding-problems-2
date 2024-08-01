1bcb0217a741511cd1f31e583c2c9b5995228367
maxgerashenko <mgerashchenko@gmail.com>
2024-08-01 01:42:02 -0400

    1 file modified

--- Blind150_BlackHard/GREED_75_2_Jump Game.ts
+++ Blind150_BlackHard/GREED_75_2_Jump Game.ts
@@ -1,3 +1,19 @@
 // Jump Game
 //
 // https://leetcode.com/problems/jump-game/description/
+
+function canJump(nums: number[]): boolean {
+  const n = nums.length;
+  let lastValidIndex = n - 1;
+
+  for (
+    let currentIndex = lastValidIndex - 1;
+    currentIndex >= 0;
+    currentIndex--
+  ) {
+    if (currentIndex + nums[currentIndex] >= lastValidIndex)
+      lastValidIndex = currentIndex; // move goal to current
+  }
+
+  return lastValidIndex === 0;
+} // T:O(n) S:O(1)
