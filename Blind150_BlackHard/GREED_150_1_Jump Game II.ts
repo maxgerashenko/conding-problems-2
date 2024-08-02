f5316d15600fcbb744bdec0531c223781c629235
maxgerashenko <mgerashchenko@gmail.com>
2024-08-01 23:46:36 -0400

    1 file modified

--- Blind150_BlackHard/GREED_150_1_Jump Game II.ts
+++ Blind150_BlackHard/GREED_150_1_Jump Game II.ts
@@ -1,3 +1,22 @@
 // Jump Game II
 //
 // https://leetcode.com/problems/jump-game-ii/description/
+function jump(nums: number[]): number {
+  let n = nums.length;
+  let l = 0;
+  let r = 0;
+  let level = 0;
+
+  while (r < n - 1) {
+    let newR = r;
+    for (let i = l; i <= r; i++) {
+      let step = nums[i];
+      newR = Math.max(newR, i + step);
+    }
+    l = r + 1;
+    r = newR;
+    level++;
+  }
+
+  return level;
+} // T:O(n) S:O(1)
