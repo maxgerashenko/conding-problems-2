5127867044f6e9993be94e058ac8de6b4ca0ce44
maxgerashenko <mgerashchenko@gmail.com>
2024-08-07 02:33:16 -0400

    1 file modified

--- Blind150_BlackHard/GREED_150_4_Valid Parenthesis String.ts
+++ Blind150_BlackHard/GREED_150_4_Valid Parenthesis String.ts
@@ -1,3 +1,32 @@
 // Valid Parenthesis String
 //
 // https://leetcode.com/problems/valid-parenthesis-string/description/
+
+function checkValidString(str: string): boolean {
+  let len = str.length;
+  let positiveMin = 0;
+  let max = 0;
+
+  for (let i = 0; i < len; i++) {
+    let el = str[i];
+
+    switch (el) {
+      case '(':
+        positiveMin++;
+        max++;
+        break;
+      case ')':
+        positiveMin--;
+        max--;
+        break;
+      case '*':
+        positiveMin--;
+        max++;
+        break;
+    }
+    if (max < 0) return false; // base case )**
+    if (positiveMin < 0) positiveMin = 0; // reset negative min
+  }
+
+  return positiveMin === 0;
+} // T:O(n) S:O(1)
