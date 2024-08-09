04bcfc42cc239bdc7b0a297804355c8463df1711
maxgerashenko <mgerashchenko@gmail.com>
2024-08-09 00:09:45 -0400

    1 file modified

--- Blind150_BlackHard/INTERVAL_75_2_Merge Intervals.ts
+++ Blind150_BlackHard/INTERVAL_75_2_Merge Intervals.ts
@@ -1,3 +1,23 @@
 // Merge Intervals
 //
 // https://leetcode.com/problems/merge-intervals/description/
+
+function merge(intervals: number[][]): number[][] {
+  intervals.sort((a, b) => a[0] - b[0]);
+  let result = [];
+
+  let [preStart, preEnd] = intervals[0];
+  for (let [start, end] of intervals) {
+    if (preEnd < start) {
+      result.push([preStart, preEnd]);
+      preStart = start;
+      preEnd = end;
+    }
+
+    preStart = Math.min(preStart, start);
+    preEnd = Math.max(preEnd, end);
+  }
+  result.push([preStart, preEnd]);
+
+  return result;
+} // T:O(NlogN) S:O(1)
