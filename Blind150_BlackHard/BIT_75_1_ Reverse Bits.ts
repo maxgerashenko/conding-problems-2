b67cdfdba247ec2204c34c9b1f65e8f7c687503b
maxgerashenko <mgerashchenko@gmail.com>
2024-08-20 02:14:01 -0400

    1 file modified

--- Blind150_BlackHard/BIT_75_1_ Reverse Bits.ts
+++ Blind150_BlackHard/BIT_75_1_ Reverse Bits.ts
@@ -1,3 +1,16 @@
 // Reverse Bits
 //
 // https://leetcode.com/problems/reverse-bits/description/
+
+// take new bit from n the very right position
+// put new bit to res very right pos
+function reverseBits(n: number): number {
+  let res = 0;
+
+  for (let i = 0; i <= 31; i++) {
+    let bit = (n >> i) & 1; // Extract the ith bit from the right
+    res |= bit << (31 - i); // Move the bit to its reversed position and set it in the result
+  }
+
+  return res >>> 0; // fix negative
+} // T:O(1) S:O(1)
