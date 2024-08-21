f0d6dec517afe7b6598502a80c1fa53ef2ef8321
maxgerashenko <mgerashchenko@gmail.com>
2024-08-20 23:16:04 -0400

    1 file modified

--- Blind150_BlackHard/BIT_75_2_Missing Number.ts
+++ Blind150_BlackHard/BIT_75_2_Missing Number.ts
@@ -1,3 +1,15 @@
 // https://leetcode.com/problems/missing-number/description/
 //
 // Missing Number
+
+function missingNumber(nums: number[]): number {
+  let len = nums.length;
+  let sum = 0;
+
+  for (let i = 0; i < len; i++) {
+    sum ^= (i + 1) ^ nums[i];
+    sum += i + 1 - nums[i];
+  }
+
+  return sum;
+} // T:(n) S:O(n)
