5790850457cd6905103246a61111587198689762
maxgerashenko <mgerashchenko@gmail.com>
2024-08-23 01:24:44 -0400

    1 file modified

---
+++ BLINK_150_GRAPH/BIT_3_Sum of Two Integers.ts
@@ -0,0 +1,17 @@
+// Sum of Two Integers
+//
+// https://leetcode.com/problems/sum-of-two-integers/description/
+
+// xor is sum
+// & << 1 is carry
+function getSum(num1: number, num2: number): number {
+  let sum = num1 ^ num2;
+  let carry = (num1 & num2) << 1;
+  while (carry !== 0) {
+    let tmp = sum;
+    sum = sum ^ carry;
+    carry = (tmp & carry) << 1;
+  }
+
+  return sum;
+} // T:O(1) S:O(1)
