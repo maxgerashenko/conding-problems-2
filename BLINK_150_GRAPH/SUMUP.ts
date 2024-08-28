b26800d459a6380a15d77cc94f4e34bfd282e201
maxgerashenko <mgerashchenko@gmail.com>
2024-08-28 02:05:15 -0400

    1 file modified

--- BLINK_150_GRAPH/SUMUP.ts
+++ BLINK_150_GRAPH/SUMUP.ts
@@ -5,3 +5,10 @@
 
 // MATH
 // % 10 and ~~(x/10) get sing digit from number
+// Matrix with 2 steps + TMP for the coner
+// split x^n in laft to get logN
+// product of each digit to [52] of array, the carry
+// square use diagonals + 2 dots and points count Map * to get resuslt
+
+// INTERVALS
+// sort start + heap end and clen to get max count
