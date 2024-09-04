17c91c9decad27d77738c0813ad6aaa74e940ea5
maxgerashenko <mgerashchenko@gmail.com>
2024-09-04 03:05:08 -0400

    1 file modified

---
+++ BLINK_150_GRAPH/GREEDY_2_Jump Game.ts
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/jump-game/description/
+//
+// Jump Game
+
+// move target to left as right is valud
+function canJump(nums: number[]): boolean {
+  let len = nums.length;
+  let validIndex = len - 1;
+
+  for (let i = len - 2; i >= 0; i--) {
+    if (i + nums[i] >= validIndex) {
+      validIndex = i;
+    }
+  }
+
+  return validIndex === 0;
+} // T:O(n) S:O(1)
