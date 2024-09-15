ec24fe66f2cd28781fe8ca2ab9d140f8f1dc9c8e
maxgerashenko <mgerashchenko@gmail.com>
2024-09-15 00:09:37 -0400

    up

--- Blind150_BlackHard/DYN2_150_5_Burst Balloons.ts
+++ Blind150_BlackHard/DYN2_150_5_Burst Balloons.ts
@@ -1,43 +1,40 @@
-// Burst Balloons
+// https://leetcode.com/problems/burst-balloons/submissions/1390631825/
 //
-// https://leetcode.com/problems/burst-balloons/description/
+// Burst Balloons
 
-// [
-//   [ 0, 0, 3, 30, 159, 167 ],
-//   [ 0, 0, 0, 15, 135, 159 ],
-//   [ 0, 0, 0, 0,  40,  48 ],
-//   [ 0, 0, 0, 0,  0,   40 ],
-//   [ 0, 0, 0, 0,  0,   0 ],
-//   [ 0, 0, 0, 0,  0,   0 ]
-// ]
+// included in the actual bursting
+// valid starting positions
+// i is an actual index that can be burst
+// bottom-up DP approach
+// dp[left][right] is the maximum coins for subarrays
+
+// bottom up solution
+// diagonal dp matrix
+// protect bounderies with pre/post injections
 function maxCoins(nums: number[]): number {
-  let coins = [1, ...nums, 1]; // extend for
-  let len = coins.length;
-  let dp = Array(len)
-    .fill(null)
-    .map((el) => Array(len).fill(0)); // n*n
+  const pNums = [1, ...nums, 1];
+  const length = pNums.length;
+  const dp = Array(length).fill(0).map(() => Array(length).fill(0));
 
-  for (
-    let length = 2;
-    length < len;
-    length++ // could start from 0, does not matter
-  )
-    for (let left = 0; left < len - length; left++) {
-      let right = left + length;
-      for (
-        let i = left + 1;
-        i < right;
-        i++ // use actuel from 1 before and after
-      )
-        dp[left][right] = Math.max(
-          dp[left][right], // max value
-          dp[left][i] + // plus rest on the left
-            coins[left] * // product  left boundary
-              coins[i] * // product last coin
-              coins[right] + // product right boundary
-            dp[i][right] // plus rest on the right
-        );
-    }
+  for (let len = 2;
+      len < length; // actual 1
+      len++) {
+      for (let left = 0;
+          left < length - len;
+          left++) {
+          const right = left + len;
+          for (let i = left + 1;
+              i < right;
+              i++) {
+              dp[left][right] = Math.max(
+                  dp[left][right],
+                  dp[left][i] +
+                  pNums[left] * pNums[i] * pNums[right] +
+                  dp[i][right]
+              );
+          }
+      }
+  }
 
-  return dp[0][len - 1]; // top right conner is the aswer
-} // T:O(N^3) S:O(N^2)
+  return dp[0][length - 1]; // solution is top right value in the matrix
+}; // T:O(n^3)  S:O(n^2)
