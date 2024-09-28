9b3edb45895353fd5bde249c5b8a726ade1ae9c3
maxgerashenko <mgerashchenko@gmail.com>
2024-09-28 02:49:32 -0400

    up

---
+++ BLINK_150_GRAPH/DYN_5_Partition Equal Subset Sum.ts
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/partition-equal-subset-sum/
+// 
+// Partition Equal Subset
+
+function canPartition(nums: number[]): boolean {
+    let totalSum = nums.reduce((pre, el) => pre + el, 0);
+
+    if (totalSum % 2 === 1) return false;
+    let target = totalSum / 2;
+    let sumsSet = new Set([0]); // base case !!!
+
+    for (let num of nums) {
+        let tmp = [...sumsSet]; // don't modify on fly
+        for (let sum of tmp) {
+            let newSum = sum + num;
+            if (newSum === target) return true;
+            if (newSum > target) continue;
+            sumsSet.add(newSum);
+        }
+    }
+
+    return false
+}; // T:O(n*sum) S:O(sum)
