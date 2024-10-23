38478ac6001de292899c85835d9358975b7f737f
maxgerashenko <mgerashchenko@gmail.com>
2024-10-23 02:27:04 -0400

    up

--- BLINK_150_GRAPH/GRAPH_1_Pacific Atlantic Water Flow.ts
+++ BLINK_150_GRAPH/GRAPH_1_Pacific Atlantic Water Flow.ts
@@ -6,56 +6,59 @@
 
 function pacificAtlantic(heights: number[][]): number[][] {
     const [m, n] = [heights.length, heights[0].length];
+    const [topLeftSet, bottomRightSet] = [new Set(), new Set()];
+    const [toLeftQueue, bottomRightQueue] = [[], []];
     const dir = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-    let [bottomRightSet, topLeftSet] = [new Set<string>(), new Set<string>()];
-    let [bottomRightQueue, topLeftQueue] = [[], []];
 
-    // Init queues and sets
+    // init sets & queues
     for (let j = 0; j < m; j++) {
-        bottomRightQueue.push([j, 0]);
-        bottomRightSet.add(`${j},0`);
-        topLeftQueue.push([j, n - 1]);
-        topLeftSet.add(`${j},${n - 1}`);
-    }
+        // left
+        topLeftSet.add(`${j},0`);
+        toLeftQueue.push([j, 0]);
+        // right
+        bottomRightSet.add(`${j},${n - 1}`);
+        bottomRightQueue.push([j, n - 1]);
+    };
+
     for (let i = 0; i < n; i++) {
-        bottomRightQueue.push([0, i]);
-        bottomRightSet.add(`0,${i}`);
-        topLeftQueue.push([m - 1, i]);
-        topLeftSet.add(`${m - 1},${i}`);
+        // top
+        topLeftSet.add(`0,${i}`);
+        toLeftQueue.push([0, i]);
+        // bottom
+        bottomRightSet.add(`${m - 1},${i}`);
+        bottomRightQueue.push([m - 1, i]);
     }
 
-    function BFS(queue: number[][], set: Set<string>) {
+    function bfs(queue, set) {
         while (queue.length > 0) {
-            let tmp = [];
+            const tmp = [];
             for (let [j, i] of queue) {
-                for (const [dj, di] of dir) {
-                    const newJ = j + dj;
-                    const newI = i + di;
+                for (let [dJ, dI] of dir) {
+                    const [newJ, newI] = [j + dJ, i + dI];
                     const key = `${newJ},${newI}`;
                     if (newJ < 0 || newI < 0 || newJ >= m || newI >= n) continue;
                     if (set.has(key)) continue;
-                    if (heights[newJ][newI] < heights[j][i]) continue;
+                    if (heights[newJ][newI] < heights[j][i]) continue; // base case
                     set.add(key);
                     tmp.push([newJ, newI]);
                 }
             }
-            queue = tmp;
+            queue = [...tmp];
         }
     }
 
-    // Perform BFS for both oceans
-    BFS(bottomRightQueue, bottomRightSet);
-    BFS(topLeftQueue, topLeftSet);
+    bfs(toLeftQueue, topLeftSet);
+    bfs(bottomRightQueue, bottomRightSet);
 
-    // Find intersection
-    const res: number[][] = [];
+    const res = [];
     for (let j = 0; j < m; j++) {
         for (let i = 0; i < n; i++) {
             const key = `${j},${i}`;
-            if (!topLeftSet.has(key) || !bottomRightSet.has(key)) continue;
-            res.push([j, i]);
+            if (topLeftSet.has(key) && bottomRightSet.has(key)) {
+                res.push([j, i]);
+            }
         }
     }
 
     return res;
-}; // T: O(m * n), S: O(m * n)
+} // T:O(n*m) S:O(m*m)
