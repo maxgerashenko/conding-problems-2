9ecad7d466ada639c5821896c2b49ac5d9177d18
maxgerashenko <mgerashchenko@gmail.com>
2024-11-27 02:28:58 -0500

    up

---
+++ BLINK_150_GRAPH/STACK_2_Largest Rectangle in Histogram.ts
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/largest-rectangle-in-histogram/
+//
+// Largest Rectangle in Histogram
+
+// Stack mono
+// {start, height}
+// cal the rest
+function largestRectangleArea(heights) {
+    let max = 0;
+    const stack = [];
+
+    for (let i = 0; i < heights.length; i++) {
+        let start = i;
+        while (
+            stack.length > 0 &&
+            heights[i] <= stack[stack.length - 1].height
+        ) {
+            const { index, height } = stack.pop();
+            max = Math.max(max, height * (i - index));
+            start = index;
+        }
+        stack.push({ index: start, height: heights[i] });
+    }
+
+    for (const { index, height } of stack) {
+        max = Math.max(max, height * (heights.length - index));
+    }
+    return max;
+} // T:O(N) S:O(n)
